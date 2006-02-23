aedf14eaacfbf1df801bfa46089d8a0b7f577784
Guido Tack <tack@gecode.org>
2006-02-23 09:20:08 +0000

    Baseline for memory benchmarks.
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@3015 e85b7adc-8362-4630-8c63-7469d557c915

---
+++ examples/baseline.cc
@@ -0,0 +1,72 @@
+/*
+ *  Main authors:
+ *     Christian Schulte <schulte@gecode.org>
+ *
+ *  Copyright:
+ *     Christian Schulte, 2001
+ *
+ *  Last modified:
+ *     $Date: 2005-11-04 15:30:42 +0100 (Fri, 04 Nov 2005) $ by $Author: schulte $
+ *     $Revision: 2499 $
+ *
+ *  This file is part of Gecode, the generic constraint
+ *  development environment:
+ *     http://www.gecode.org
+ *
+ *  See the file "LICENSE" for information on usage and
+ *  redistribution of this file, and for a
+ *     DISCLAIMER OF ALL WARRANTIES.
+ *
+ */
+
+#include "examples/support.hh"
+
+/**
+ * \brief %Example: Baseline test
+ *
+ * \ingroup Example
+ *
+ */
+class Baseline : public Example {
+protected:
+  /// Variables
+  IntVarArray x;
+public:
+  /// The actual problem
+  Baseline(const Options& opt)
+    : x(this,1,0,1) {
+    branch(this, x, BVAR_NONE, BVAL_MIN);
+  }
+
+  /// Constructor for cloning \a s
+  Baseline(bool share, Baseline& s) : Example(share,s) {
+    x.update(this, share, s.x);
+  }
+
+  /// Perform copying during cloning
+  virtual Space*
+  copy(bool share) {
+    return new Baseline(share,*this);
+  }
+
+  /// Print solution
+  virtual void
+  print(void) {}
+};
+
+/** \brief Main-function
+ *  \relates Baseline
+ */
+int
+main(int argc, char** argv) {
+  Options opt("Baseline");
+  opt.iterations = 2000;
+  opt.c_d        = 5;
+  opt.solutions  = 0;
+  opt.parse(argc,argv);
+  Example::run<Baseline,DFS>(opt);
+  return 0;
+}
+
+// STATISTICS: example-any
+
