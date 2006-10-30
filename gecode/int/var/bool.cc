b1039694f173fcb1509202fc75c72ec89d14a2f2
Christian Schulte <schulte@gecode.org>
2006-10-30 16:56:51 +0000

    Uuups
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@3822 e85b7adc-8362-4630-8c63-7469d557c915

---
+++ gecode/int/var/bool.cc
@@ -0,0 +1,47 @@
+/* -*- mode: C++; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+/*
+ *  Main authors:
+ *     Christian Schulte <schulte@gecode.org>
+ *
+ *  Copyright:
+ *     Christian Schulte, 2006
+ *
+ *  Last modified:
+ *     $Date: 2006-10-25 15:21:37 +0200 (Wed, 25 Oct 2006) $ by $Author: tack $
+ *     $Revision: 3791 $
+ *
+ *  This file is part of Gecode, the generic constraint
+ *  development environment:
+ *     http://www.gecode.org
+ *
+ *  See the file "LICENSE" for information on usage and
+ *  redistribution of this file, and for a
+ *     DISCLAIMER OF ALL WARRANTIES.
+ *
+ */
+
+#include "gecode/int.hh"
+
+namespace Gecode {
+
+  BoolVar::BoolVar(Space* home, int min, int max)
+    : var(new (home) Int::IntVarImp(home,min,max)) {
+    if ((min < 0) || (max > 1))
+      throw Int::VariableOutOfRangeDomain("BoolVar::BoolVar");
+    if (min > max)
+      throw Int::VariableEmptyDomain("BoolVar::BoolVar");
+  }
+
+  void
+  BoolVar::init(Space* home, int min, int max) {
+    if ((min < 0) || (max > 1))
+      throw Int::VariableOutOfRangeDomain("BoolVar::init");
+    if (min > max)
+      throw Int::VariableEmptyDomain("BoolVar::init");
+    var = new (home) Int::IntVarImp(home,min,max);
+  }
+
+}
+
+// STATISTICS: int-var
+
