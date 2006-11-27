cbe2e0860c3fc0f2020fbf047aaa686ca30b7a74
Christian Schulte <schulte@gecode.org>
2006-11-27 21:14:07 +0000

    Some tweaks for Boolean vars
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@4004 e85b7adc-8362-4630-8c63-7469d557c915

--- gecode/int/var/bool.cc
+++ gecode/int/var/bool.cc
@@ -24,12 +24,17 @@
 
 namespace Gecode {
 
-  BoolVar::BoolVar(Space* home, int min, int max)
-    : var(new (home) Int::BoolVarImp(home,min,max)) {
+  BoolVar::BoolVar(Space* home, int min, int max) {
     if ((min < 0) || (max > 1))
       throw Int::VariableOutOfRangeDomain("BoolVar::BoolVar");
     if (min > max)
       throw Int::VariableEmptyDomain("BoolVar::BoolVar");
+    if (min > 0)
+      var = &Int::BoolVarImp::s_one;
+    else if (max == 0)
+      var = &Int::BoolVarImp::s_zero;
+    else
+      var = new (home) Int::BoolVarImp(home,0,1);
   }
 
   void
@@ -38,7 +43,12 @@ namespace Gecode {
       throw Int::VariableOutOfRangeDomain("BoolVar::init");
     if (min > max)
       throw Int::VariableEmptyDomain("BoolVar::init");
-    var = new (home) Int::BoolVarImp(home,min,max);
+    if (min > 0)
+      var = &Int::BoolVarImp::s_one;
+    else if (max == 0)
+      var = &Int::BoolVarImp::s_zero;
+    else
+      var = new (home) Int::BoolVarImp(home,0,1);
   }
 
 }
