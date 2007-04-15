d7cea548d1bc51cbad0c68deecec6c3b1cb635ae
Christian Schulte <schulte@gecode.org>
2007-04-15 20:20:39 +0000

    Fixed regular (but still not fixpoint correct...)
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@4403 e85b7adc-8362-4630-8c63-7469d557c915

--- test/int/regular.cc
+++ test/int/regular.cc
@@ -23,86 +23,134 @@
 #include "test/int.hh"
 #include "test/log.hh"
 
-static IntSet ds_22(-2,2);
-
-class RegularA : public IntTest {
-public:
-  RegularA(const char* t)
-    : IntTest(t,4,ds_22,false,ICL_DOM) {}
-  virtual bool solution(const Assignment& x) const {
-    return (((x[0] == 0) || (x[0] == 2)) &&
-            ((x[1] == -1) || (x[1] == 1)) &&
-            ((x[2] == 0) || (x[2] == 1)) &&
-            ((x[3] == 0) || (x[3] == 1)));
-  }
-  virtual void post(Space* home, IntVarArray& x) {
-    REG r =
-      (REG(0) | REG(2)) +
-      (REG(-1) | REG(1)) +
-      (REG(7) | REG(0) | REG(1)) +
-      (REG(0) | REG(1));
-    DFA d(r);
-    regular(home, x, d);
-  }
-};
-
-class RegularB : public IntTest {
-public:
-  RegularB(const char* t)
-    : IntTest(t,4,ds_22,false,ICL_DOM) {}
-  virtual bool solution(const Assignment& x) const {
-    return (x[0]<x[1]) && (x[1]<x[2]) && (x[2]<x[3]);
-  }
-  virtual void post(Space* home, IntVarArray& x) {
-    REG r =
-      (REG(-2) + REG(-1) + REG(0) + REG(1)) |
-      (REG(-2) + REG(-1) + REG(0) + REG(2)) |
-      (REG(-2) + REG(-1) + REG(1) + REG(2)) |
-      (REG(-2) + REG(0) + REG(1) + REG(2)) |
-      (REG(-1) + REG(0) + REG(1) + REG(2));
-    DFA d(r);
-    regular(home, x, d);
-  }
-};
-
-class RegularShared : public IntTest {
-public:
-  RegularShared(const char* t)
-    : IntTest(t,2,ds_22,false,ICL_DOM) {}
-  virtual bool solution(const Assignment& x) const {
-    return (((x[0] == 0) || (x[0] == 2)) &&
-            ((x[1] == -1) || (x[1] == 1)) &&
-            ((x[0] == 0) || (x[0] == 1)) &&
-            ((x[1] == 0) || (x[1] == 1)));
-  }
-  virtual void post(Space* home, IntVarArray& x) {
-     REG r =
-      (REG(0) | REG(2)) +
-      (REG(-1) | REG(1)) +
-      (REG(7) | REG(0) | REG(1)) +
-      (REG(0) | REG(1));
-    DFA d(r);
-    IntVarArgs y(4);
-    y[0]=x[0]; y[1]=x[1]; y[2]=x[0]; y[3]=x[1];
-    regular(home, y, d);
-  }
-  virtual void description(std::ostream& h, std::ostream& c) {
-    h << "post regular: x[0]x[1]x[0]x[1] in (0|2)(-1|1)(7|0|1)(0|1)" << std::endl;
-    c << "\tREG r = \n"
-      << "\t  (REG(0) | REG(2)) +\n"
-      << "\t  (REG(-1) | REG(1)) +\n"
-      << "\t  (REG(7) | REG(0) | REG(1)) +\n"
-      << "\t  (REG(0) | REG(1));\n"
-      << "\tDFA d(r);\n"
-      << "\tIntVarArgs y(4);\n"
-      << "\ty[0]=x[0]; y[1]=x[1]; y[2]=x[0]; y[3]=x[1];\n"
-      << "\tregular(home, y, d);\n" << std::endl;
-  }
-};
 namespace {
+
+  IntSet ds_22(-2,2);
+  IntSet ds_14(-1,4);
+
+  REG ra = ((REG(0) | REG(2)) +
+            (REG(-1) | REG(1)) +
+            (REG(7) | REG(0) | REG(1)) +
+            (REG(0) | REG(1)));
+  DFA da(ra);
+
+  class RegularA : public IntTest {
+  public:
+    RegularA(const char* t)
+      : IntTest(t,4,ds_22,false,ICL_DOM) {}
+    virtual bool solution(const Assignment& x) const {
+      return (((x[0] == 0) || (x[0] == 2)) &&
+              ((x[1] == -1) || (x[1] == 1)) &&
+              ((x[2] == 0) || (x[2] == 1)) &&
+              ((x[3] == 0) || (x[3] == 1)));
+    }
+    virtual void post(Space* home, IntVarArray& x) {
+      regular(home, x, da);
+    }
+  };
+  
+  REG rb = ((REG(-2) + REG(-1) + REG(0) + REG(1)) |
+            (REG(-2) + REG(-1) + REG(0) + REG(2)) |
+            (REG(-2) + REG(-1) + REG(1) + REG(2)) |
+            (REG(-2) + REG(0) + REG(1) + REG(2)) |
+            (REG(-1) + REG(0) + REG(1) + REG(2)));
+  DFA db(rb);
+
+  class RegularB : public IntTest {
+  public:
+    RegularB(const char* t)
+      : IntTest(t,4,ds_22,false,ICL_DOM) {}
+    virtual bool solution(const Assignment& x) const {
+      return (x[0]<x[1]) && (x[1]<x[2]) && (x[2]<x[3]);
+    }
+    virtual void post(Space* home, IntVarArray& x) {
+      regular(home, x, db);
+    }
+  };
+  
+  REG rd = ((REG(0)+REG(1)+REG(2)+REG(3)) |
+            (REG(0)+REG(1)+REG(3)+REG(2)) |
+            (REG(0)+REG(2)+REG(1)+REG(3)) |
+            (REG(0)+REG(2)+REG(3)+REG(1)) |
+            (REG(0)+REG(3)+REG(1)+REG(2)) |
+            (REG(0)+REG(3)+REG(2)+REG(1)) |
+            (REG(1)+REG(0)+REG(2)+REG(3)) |
+            (REG(1)+REG(0)+REG(3)+REG(2)) |
+            (REG(1)+REG(2)+REG(0)+REG(3)) |
+            (REG(1)+REG(2)+REG(3)+REG(0)) |
+            (REG(1)+REG(3)+REG(0)+REG(2)) |
+            (REG(1)+REG(3)+REG(2)+REG(0)) |
+            (REG(2)+REG(0)+REG(1)+REG(3)) |
+            (REG(2)+REG(0)+REG(3)+REG(1)) |
+            (REG(2)+REG(1)+REG(0)+REG(3)) |
+            (REG(2)+REG(1)+REG(3)+REG(0)) |
+            (REG(2)+REG(3)+REG(0)+REG(1)) |
+            (REG(2)+REG(3)+REG(1)+REG(0)) |
+            (REG(3)+REG(0)+REG(1)+REG(2)) |
+            (REG(3)+REG(0)+REG(2)+REG(1)) |
+            (REG(3)+REG(1)+REG(0)+REG(2)) |
+            (REG(3)+REG(1)+REG(2)+REG(0)) |
+            (REG(3)+REG(2)+REG(0)+REG(1)) |
+            (REG(3)+REG(2)+REG(1)+REG(0)));
+
+  DFA dd(rd);
+
+  class RegularDistinct : public IntTest {
+  public:
+    RegularDistinct(const char* t)
+      : IntTest(t,4,ds_14,false,ICL_DOM) {}
+    virtual bool solution(const Assignment& x) const {
+      for (int i=0; i<x.size(); i++) {
+        if ((x[i] < 0) || (x[i] > 3))
+          return false;
+        for (int j=i+1; j<x.size(); j++)
+          if (x[i]==x[j])
+            return false;
+      }
+      return true;
+    }
+    virtual void post(Space* home, IntVarArray& x) {
+      regular(home, x, dd);
+    }
+  };
+  
+  class RegularShared : public IntTest {
+  public:
+    RegularShared(const char* t)
+      : IntTest(t,2,ds_22,false,ICL_DOM) {}
+    virtual bool solution(const Assignment& x) const {
+      return (((x[0] == 0) || (x[0] == 2)) &&
+              ((x[1] == -1) || (x[1] == 1)) &&
+              ((x[0] == 0) || (x[0] == 1)) &&
+              ((x[1] == 0) || (x[1] == 1)));
+    }
+    virtual void post(Space* home, IntVarArray& x) {
+      IntVarArgs y(4);
+      y[0]=x[0]; y[1]=x[1]; y[2]=x[0]; y[3]=x[1];
+      regular(home, y, da);
+    }
+    virtual void description(std::ostream& h, std::ostream& c) {
+      h << "post regular: x[0]x[1]x[0]x[1] in (0|2)(-1|1)(7|0|1)(0|1)" << std::endl;
+      c << "\tREG r = \n"
+        << "\t  (REG(0) | REG(2)) +\n"
+        << "\t  (REG(-1) | REG(1)) +\n"
+        << "\t  (REG(7) | REG(0) | REG(1)) +\n"
+        << "\t  (REG(0) | REG(1));\n"
+        << "\tDFA d(r);\n"
+        << "\tIntVarArgs y(4);\n"
+        << "\ty[0]=x[0]; y[1]=x[1]; y[2]=x[0]; y[3]=x[1];\n"
+        << "\tregular(home, y, d);\n" << std::endl;
+    }
+  };
+
+
   RegularA _rega("Regular::A");
   RegularB _regb("Regular::B");
-  RegularShared _regsa("Regular::Shared");
+
+  RegularDistinct _regd("Regular::Distinct");
+
+  RegularShared _regs("Regular::Shared");
+
 }
 
 // STATISTICS: test-int
