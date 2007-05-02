e6a1f0f8e2e666eb69c63aee7d768679ab78d028
Christian Schulte <schulte@gecode.org>
2007-05-02 12:33:01 +0000

    Cleaned and updated
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@4522 e85b7adc-8362-4630-8c63-7469d557c915

--- examples/queens.cc
+++ examples/queens.cc
@@ -23,115 +23,6 @@
 #include "examples/support.hh"
 #include "gecode/minimodel.hh"
 
-#if defined(BINLIN_NQ_ADVISOR_BASE) || defined(BINLIN_NQ_ADVISOR_AVOID)
-
-using namespace Int;
-
-  class NQC : public Propagator {
-  protected:
-    IntView x0;
-    IntView x1;
-    int c;
-    Council<ViewAdvisor<IntView> > co;
-    /// Constructor for cloning \a p
-    NQC(Space* home, bool share, NQC& p);
-    /// Constructor for creation
-    NQC(Space* home, IntView x0, IntView x1, int c);
-  public:
-    virtual ExecStatus 
-    advise(Space* home, Advisor& _a, const Delta& d) {
-      ViewAdvisor<IntView>& a = static_cast<ViewAdvisor<IntView>&>(_a);
-#if defined(BINLIN_NQ_ADVISOR_BASE)
-      if (IntView::modevent(d) == ME_INT_VAL)
-        return ES_SUBSUMED_NOFIX(a,home);
-      return ES_NOFIX;
-#endif
-#if defined(BINLIN_NQ_ADVISOR_AVOID)
-      if (IntView::modevent(d) == ME_INT_VAL)
-        return ES_SUBSUMED_NOFIX(a,home);
-      return ES_FIX;
-#endif
-    }
-    /// Create copy during cloning
-    virtual Actor* copy(Space* home, bool share);
-    /// Perform propagation
-    virtual ExecStatus propagate(Space* home);
-    /// Cost function (defined as PC_UNARY_LO)
-    virtual PropCost cost(void) const;
-    /// Post propagator for \f$x_0+x_1 \neq c\f$
-    static ExecStatus post(Space* home, IntView x0, IntView x1, int c);
-    /// Delete propagator and return its size
-    virtual size_t dispose(Space* home);
-  };
-
-  forceinline
-  NQC::NQC(Space* home, IntView _x0, IntView _x1, int _c)
-    : Propagator(home), x0(_x0), x1(_x1), c(_c), co(home,2) {
-    new (home) ViewAdvisor<IntView>(home,this,co,x0);
-    new (home) ViewAdvisor<IntView>(home,this,co,x1);
-    if (x0.assigned() || x1.assigned())
-      IntView::schedule(home,this,ME_INT_VAL);
-  }
-
-  ExecStatus
-  NQC::post(Space* home, IntView x0, IntView x1, int c) {
-    /*
-    if (x0.assigned()) {
-      GECODE_ME_CHECK(x1.nq(home,c-x0.val()));
-    } else if (x1.assigned()) {
-      GECODE_ME_CHECK(x0.nq(home,c-x1.val()));
-    } else {
-      (void) new (home) NQC(home,x0,x1,c);
-    }
-    */
-    (void) new (home) NQC(home,x0,x1,c);
-    return ES_OK;
-  }
-
-
-  forceinline size_t
-  NQC::dispose(Space* home) {
-    assert(!home->failed());
-    co.dispose(home);
-    (void) Propagator::dispose(home);
-    return sizeof(*this);
-  }
-
-
-  forceinline
-  NQC::NQC(Space* home, bool share, NQC& p)
-    : Propagator(home,share,p), c(p.c), co(home,share,p.co) {
-    x0.update(home,share,p.x0);
-    x1.update(home,share,p.x1);
-  }
-  
-  Actor*
-  NQC::copy(Space* home, bool share) {
-    return new (home) NQC(home,share,*this);
-  }
-
-  PropCost
-  NQC::cost(void) const {
-    return PC_UNARY_LO;
-  }
-
-  ExecStatus
-  NQC::propagate(Space* home) {
-    if (x0.assigned()) {
-      GECODE_ME_CHECK(x1.nq(home,x0.val()-c));
-      return ES_SUBSUMED(this,home);
-    } 
-    if (x1.assigned()) {
-      GECODE_ME_CHECK(x0.nq(home,c+x1.val()));
-      return ES_SUBSUMED(this,home);
-    }
-    return ES_FIX;
-  }
-
-
-#endif
-
-
 /**
  * \brief %Example: n-%Queens puzzle
  *
@@ -154,13 +45,8 @@ public:
       for (int i = 0; i<n; i++)
         for (int j = i+1; j<n; j++) {
           post(this, q[i] != q[j]);
-#if defined(BINLIN_NQ_ADVISOR_BASE) || defined(BINLIN_NQ_ADVISOR_AVOID)
-          NQC::post(this, q[i], q[j], j-i); 
-          NQC::post(this, q[i], q[j], i-j); 
-#else
           post(this, q[i]+i != q[j]+j);
           post(this, q[i]-i != q[j]-j);
-#endif
         }
     } else {
       IntArgs c(n);
