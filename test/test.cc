456e2e4362ad53d3cf87ff98d8eadfe55982f7f4
Christian Schulte <schulte@gecode.org>
2007-05-21 15:23:17 +0000

    Not working
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@4618 e85b7adc-8362-4630-8c63-7469d557c915

--- test/test.cc
+++ test/test.cc
@@ -233,7 +233,7 @@ Options::parse(int argc, char** argv) {
     }
     i++;
   }
-  //  testpat.push_back(make_pair(false, "Channel::Bool"));
+  //  testpat.push_back(make_pair(false, "Distinct::Dom"));
   return;
  missing:
   e = "missing parameter";
