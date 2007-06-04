c4a79001f530448a54b9f4fae0200be444bf77f0
Christian Schulte <schulte@gecode.org>
2007-06-04 12:02:24 +0000

    Works
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@4655 e85b7adc-8362-4630-8c63-7469d557c915

--- test/test.cc
+++ test/test.cc
@@ -233,7 +233,7 @@ Options::parse(int argc, char** argv) {
     }
     i++;
   }
-  testpat.push_back(make_pair(false, "Regular"));
+  testpat.push_back(make_pair(false, "Regular::Shared"));
   return;
  missing:
   e = "missing parameter";
