b9a917c38d053a3bdc9cb1ae022fd629b98ff47c
Christian Schulte <schulte@gecode.org>
2007-10-29 19:53:18 +0000

    Compiles (wrong namespace)
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@5141 e85b7adc-8362-4630-8c63-7469d557c915

--- gecode/int/extensional.hh
+++ gecode/int/extensional.hh
@@ -145,7 +145,7 @@ namespace Gecode { namespace Int { namespace Extensional {
     static void post(Space* home, Reflection::VarMap& vars,
                      const Reflection::ActorSpec& spec);
     /// Mangled name of this propagator
-    static Support::Symbol name(void);
+    static Gecode::Support::Symbol name(void);
     /// Delete propagator and return its size
     virtual size_t dispose(Space* home);
     /// Post propagator on views \a x and DFA \a d
