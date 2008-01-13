b23be3a341e0d3ac9cccae6eea9da15b476c3ec1
Christian Schulte <schulte@gecode.org>
2008-01-13 14:11:27 +0000

    Properties
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@5863 e85b7adc-8362-4630-8c63-7469d557c915

--- misc/fixproperties.sh
+++ misc/fixproperties.sh
@@ -38,7 +38,7 @@
 set -e
 
 # List of file extensions for which properties should be set
-KEYWORDEXTS="cc icc hh sh perl ac in"
+KEYWORDEXTS="cc icc hh sh perl ac in vsl vis"
 
 for ext in ${KEYWORDEXTS}; do
     find . -name "*.$ext" ! -wholename './contribs/*' -prune \
