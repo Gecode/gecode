68f94bbe871c598638e905070533032a10129bc8
Christian Schulte <schulte@gecode.org>
2008-02-18 08:27:02 +0000

    Added missing tests
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@6206 e85b7adc-8362-4630-8c63-7469d557c915

--- test/int/rel.cc
+++ test/int/rel.cc
@@ -56,22 +56,33 @@ namespace Test { namespace Int {
        Gecode::IntRelType irt;
      public:
        /// Create and register test
-       IntBinVar(Gecode::IntRelType irt0, 
-                 Gecode::IntConLevel icl=Gecode::ICL_DEF)
-         : Test("Rel::Int::Var::"+str(irt0)+"::"+str(icl),
-                   2,-3,3,true,icl), 
+       IntBinVar(Gecode::IntRelType irt0, int n, Gecode::IntConLevel icl)
+         : Test("Rel::Int::Var::"+str(irt0)+"::"+str(icl)+"::"+str(n),
+                n+1,-3,3,n==1,icl), 
            irt(irt0) {}
        /// Test whether \a x is solution
        virtual bool solution(const Assignment& x) const {
-         return cmp(x[0],irt,x[1]);
+         if (x.size() == 2) {
+           return cmp(x[0],irt,x[1]);
+         } else {
+           return cmp(x[0],irt,x[2]) && cmp(x[1],irt,x[2]);
+         }
        }
        /// Post constraint on \a x
        virtual void post(Gecode::Space* home, Gecode::IntVarArray& x) {
-         Gecode::rel(home, x[0], irt, x[1], icl);
+         using namespace Gecode;
+         if (x.size() == 2) {
+           rel(home, x[0], irt, x[1], icl);
+         } else {
+           IntVarArgs y(2);
+           y[0]=x[0]; y[1]=x[1];
+           rel(home, y, irt, x[2], icl);
+         }
        }
        /// Post reified constraint on \a x for \a b
        virtual void post(Gecode::Space* home, Gecode::IntVarArray& x, 
                          Gecode::BoolVar b) {
+         assert(x.size() == 2);
          Gecode::rel(home, x[0], irt, x[1], b, icl);
        }
      };
@@ -83,16 +94,26 @@ namespace Test { namespace Int {
        Gecode::IntRelType irt;
      public:
        /// Create and register test
-       BoolBinVar(Gecode::IntRelType irt0) 
-         : Test("Rel::Bool::Var::"+str(irt0),2,0,1), irt(irt0) {}
+       BoolBinVar(Gecode::IntRelType irt0, int n) 
+         : Test("Rel::Bool::Var::"+str(irt0)+"::"+str(n),n+1,0,1), irt(irt0) {}
        /// Test whether \a x is solution
        virtual bool solution(const Assignment& x) const {
-         return cmp(x[0],irt,x[1]);
+         if (x.size() == 2) {
+           return cmp(x[0],irt,x[1]);
+         } else {
+           return cmp(x[0],irt,x[2]) && cmp(x[1],irt,x[2]);
+         }
        }
        /// Post constraint on \a x
        virtual void post(Gecode::Space* home, Gecode::IntVarArray& x) {
          using namespace Gecode;
-         rel(home, channel(home,x[0]), irt, channel(home,x[1]));
+         if (x.size() == 2) {
+           rel(home, channel(home,x[0]), irt, channel(home,x[1]));
+         } else {
+           Gecode::BoolVarArgs y(2);
+           y[0]=channel(home,x[0]); y[1]=channel(home,x[1]);
+           rel(home, y, irt, channel(home,x[2]));
+         }
        }
      };
    
@@ -105,20 +126,29 @@ namespace Test { namespace Int {
        int c;
      public:
        /// Create and register test
-       IntBinInt(Gecode::IntRelType irt0, int c0) 
-         : Test("Rel::Int::Int::"+str(irt0)+"::"+str(c0),1,-3,3,true), 
+       IntBinInt(Gecode::IntRelType irt0, int n, int c0) 
+         : Test("Rel::Int::Int::"+str(irt0)+"::"+str(n)+"::"+str(c0),
+                n,-3,3,n==1), 
            irt(irt0), c(c0) {}
        /// Test whether \a x is solution
        virtual bool solution(const Assignment& x) const {
-         return cmp(x[0],irt,c);
+         if (x.size() == 1)
+           return cmp(x[0],irt,c);
+         else
+           return cmp(x[0],irt,c) && cmp(x[1],irt,c);
        }
        /// Post constraint on \a x
        virtual void post(Gecode::Space* home, Gecode::IntVarArray& x) {
-         Gecode::rel(home, x[0], irt, c);
+         using namespace Gecode;
+         if (x.size() == 1)
+           rel(home, x[0], irt, c);
+         else
+           rel(home, x, irt, c);
        }
        /// Post reified constraint on \a x for \a b
        virtual void post(Gecode::Space* home, Gecode::IntVarArray& x, 
                          Gecode::BoolVar b) {
+         assert(x.size() == 1);
          Gecode::rel(home, x[0], irt, c, b);
        }
      };
@@ -132,16 +162,26 @@ namespace Test { namespace Int {
        int c;
      public:
        /// Create and register test
-       BoolBinInt(Gecode::IntRelType irt0, int c0) 
-         : Test("Rel::Bool::Int::"+str(irt0)+"::"+str(c0),1,0,1), 
+       BoolBinInt(Gecode::IntRelType irt0, int n, int c0) 
+         : Test("Rel::Bool::Int::"+str(irt0)+"::"+str(n)+"::"+str(c0),1,0,1), 
            irt(irt0), c(c0) {}
        /// Test whether \a x is solution
        virtual bool solution(const Assignment& x) const {
-         return cmp(x[0],irt,c);
+         if (x.size() == 1)
+           return cmp(x[0],irt,c);
+         else
+           return cmp(x[0],irt,c) && cmp(x[1],irt,c);
        }
        /// Post constraint on \a x
        virtual void post(Gecode::Space* home, Gecode::IntVarArray& x) {
-         Gecode::rel(home, Gecode::channel(home,x[0]), irt, c);
+         using namespace Gecode;
+         if (x.size() == 1) {
+           rel(home, channel(home,x[0]), irt, c);
+         } else {
+           BoolVarArgs y(2);
+           y[0]=channel(home,x[0]); y[1]=channel(home,x[1]);
+           rel(home, y, irt, c);
+         }
        }
      };
    
@@ -270,15 +310,21 @@ namespace Test { namespace Int {
          using namespace Gecode;
          for (IntRelTypes irts; irts(); ++irts) {
            for (IntConLevels icls; icls(); ++icls) {
-             (void) new IntBinVar(irts.irt(),icls.icl());
+             (void) new IntBinVar(irts.irt(),1,icls.icl());
+             (void) new IntBinVar(irts.irt(),2,icls.icl());
              (void) new IntPairwise(irts.irt(),icls.icl());
            }
-           (void) new BoolBinVar(irts.irt());
+           (void) new BoolBinVar(irts.irt(),1);
+           (void) new BoolBinVar(irts.irt(),2);
            (void) new BoolPairwise(irts.irt());
-           for (int c=-4; c<=4; c++)
-             (void) new IntBinInt(irts.irt(),c);
-           for (int c=0; c<=1; c++)
-             (void) new BoolBinInt(irts.irt(),c);
+           for (int c=-4; c<=4; c++) {
+             (void) new IntBinInt(irts.irt(),1,c);
+             (void) new IntBinInt(irts.irt(),2,c);
+           }
+           for (int c=0; c<=1; c++) {
+             (void) new BoolBinInt(irts.irt(),1,c);
+             (void) new BoolBinInt(irts.irt(),2,c);
+           }
            (void) new IntArray(irts.irt());
            (void) new BoolArray(irts.irt());
          }      
