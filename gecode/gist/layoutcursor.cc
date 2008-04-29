33f7b0103ac6bba2636f64e9cb12a333a9ee1234
Guido Tack <tack@gecode.org>
2008-04-29 09:38:44 +0000

    Do not compute intermediate results when doing the gist layout, but do everything in place
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@6803 e85b7adc-8362-4630-8c63-7469d557c915

--- gecode/gist/layoutcursor.cc
+++ gecode/gist/layoutcursor.cc
@@ -48,16 +48,18 @@ namespace Gecode { namespace Gist {
     /// Offsets computed
     int* offsets;
     /// Compute distance needed between \a shape1 and \a shape2
-    int getAlpha(Shape* shape1, Shape* shape2);
-    /// Merge \a shape1 and \a shape2 with distance \a alpha
-    static Shape* merge(Shape* shape1, Shape* shape2, int alpha);
+    int getAlpha(Extent* shape1, int depth1, Extent* shape2, int depth2);
+    /// Merge \a shape1 and \a shape2 into \a result with distance \a alpha
+    static void merge(Extent* result,
+                      Extent* shape1, int depth1,
+                      Extent* shape2, int depth2, int alpha);
   public:
     /// Constructor
     ShapeList(int length);
     /// Destructor
     ~ShapeList(void);
     /// Return the merged shape
-    Shape* getMergedShape(bool left = false);
+    Shape* getMergedShape(Extent extent, bool left = false);
     /// Return offset computed for child \a i
     int getOffsetOfChild(int i);
     /// Return shape no \a i
@@ -140,95 +142,94 @@ namespace Gecode { namespace Gist {
   }
   
   int
-  ShapeList::getAlpha(Shape* shape1, Shape* shape2) {
+  ShapeList::getAlpha(Extent* shape1, int depth1,
+                      Extent* shape2, int depth2) {
     int alpha = Layout::minimalSeparation;
     int extentR = 0;
     int extentL = 0;
-    int depth1 = shape1->depth();
-    int depth2 = shape2->depth();
     for (int i=0; i<depth1 && i<depth2; i++) {
-      extentR += (*shape1)[i].r;
-      extentL += (*shape2)[i].l;
+      extentR += shape1[i].r;
+      extentL += shape2[i].l;
       alpha = std::max(alpha, extentR - extentL + Layout::minimalSeparation);
     }
     return alpha;
   }
   
-  Shape*
-  ShapeList::merge(Shape* shape1, Shape* shape2, int alpha) {
-    if (shape1->depth() == 0) {
-      return new Shape(shape2);
-    } else if (shape2->depth() == 0) {
-      return new Shape(shape1);
+  void
+  ShapeList::merge(Extent* result,
+                   Extent* shape1, int depth1,
+                   Extent* shape2, int depth2, int alpha) {
+    if (depth1 == 0) {
+      for (int i=depth2; i--;)
+        result[i] = shape2[i];
+    } else if (depth2 == 0) {
+      for (int i=depth1; i--;)
+        result[i] = shape1[i];
     } else {
-      Shape* result = new Shape(std::max(shape1->depth(),shape2->depth()));
-      int extentCount = 0;
-        
       // Extend the topmost right extent by alpha.  This, in effect,
       // moves the second shape to the right by alpha units.
-      int topmostL = (*shape1)[0].l;
-      int topmostR = (*shape2)[0].r;      
-      (*result)[extentCount++] = Extent(topmostL, topmostR+alpha);
+      int topmostL = shape1[0].l;
+      int topmostR = shape2[0].r;      
+      int backoffTo1 =
+        shape1[0].r - alpha - shape2[0].r;
+      int backoffTo2 =
+        shape2[0].l + alpha - shape1[0].l;
+
+      result[0] = Extent(topmostL, topmostR+alpha);
         
       // Now, since extents are given in relative units, in order to
       // compute the extents of the merged shape, we can just collect the
       // extents of shape1 and shape2, until one of the shapes ends.  If
       // this happens, we need to "back-off" to the axis of the deeper
       // shape in order to properly determine the remaining extents.
-      int backoffTo1 =
-        (*shape1)[0].r - alpha - (*shape2)[0].r;
-      int backoffTo2 =
-        (*shape2)[0].l + alpha - (*shape1)[0].l;
       int i=1;
-      for (; i<shape1->depth() && i<shape2->depth(); i++) {
-        Extent currentExtent1 = (*shape1)[i];
-        Extent currentExtent2 = (*shape2)[i];
+      for (; i<depth1 && i<depth2; i++) {
+        Extent currentExtent1 = shape1[i];
+        Extent currentExtent2 = shape2[i];
         int newExtentL = currentExtent1.l;
         int newExtentR = currentExtent2.r;        
-        (*result)[extentCount++] = Extent(newExtentL, newExtentR);
+        result[i] = Extent(newExtentL, newExtentR);
         backoffTo1 += currentExtent1.r - currentExtent2.r;
         backoffTo2 += currentExtent2.l - currentExtent1.l;
       }
         
       // If shape1 is deeper than shape2, back off to the axis of shape1,
       // and process the remaining extents of shape1.
-      if (i<shape1->depth()) {
-        Extent currentExtent1 = (*shape1)[i];
-        ++i;
+      if (i<depth1) {
+        Extent currentExtent1 = shape1[i];
         int newExtentL = currentExtent1.l;
         int newExtentR = currentExtent1.r;        
-        (*result)[extentCount++] = Extent(newExtentL, newExtentR+backoffTo1);
-        for (; i<shape1->depth(); i++) {
-          (*result)[extentCount++] = (*shape1)[i];
+        result[i] = Extent(newExtentL, newExtentR+backoffTo1);
+        ++i;
+        for (; i<depth1; i++) {
+          result[i] = shape1[i];
         }
       }
         
       // Vice versa, if shape2 is deeper than shape1, back off to the
       // axis of shape2, and process the remaining extents of shape2.
-      if (i<shape2->depth()) {
-        Extent currentExtent2 = (*shape2)[i];
-        ++i;
+      if (i<depth2) {
+        Extent currentExtent2 = shape2[i];
         int newExtentL = currentExtent2.l;
         int newExtentR = currentExtent2.r;
-        (*result)[extentCount++] = Extent(newExtentL+backoffTo2, newExtentR);
-        for (; i<shape2->depth(); i++) {
-          (*result)[extentCount++] = (*shape2)[i];
+        result[i] = Extent(newExtentL+backoffTo2, newExtentR);
+        ++i;
+        for (; i<depth2; i++) {
+          result[i] = shape2[i];
         }
       }
-      
-      assert(extentCount == std::max(shape1->depth(), shape2->depth()));
-      return result;
     }
     
   }
   
   Shape*
-  ShapeList::getMergedShape(bool left) {
+  ShapeList::getMergedShape(Extent extent, bool left) {
     if (numberOfShapes == 1) {
       offsets[0] = 0;
-      return new Shape(shapes[0]);
+      Shape* result = new Shape(extent, shapes[0]);
+      (*result)[1].extend(- extent.l, - extent.r);
+      return result;
     } else {
-      Shape* mergedShape;
       // alphaL[] and alphaR[] store the necessary distances between the
       // axes of the shapes in the list: alphaL[i] gives the distance
       // between shape[i] and shape[i-1], when shape[i-1] and shape[i]
@@ -240,9 +241,24 @@ namespace Gecode { namespace Gist {
         
       // distance between the leftmost and the rightmost axis in the list
       int width = 0;
+
+      int maxDepth = 0;
+      for (int i = numberOfShapes; i--;)
+        maxDepth = std::max(maxDepth, shapes[i]->depth());
         
-      Shape* currentShapeL = new Shape(shapes[0]);
-      Shape* currentShapeR = new Shape(shapes[numberOfShapes - 1]);
+      GECODE_AUTOARRAY(Extent, currentShapeL, maxDepth);
+      int ldepth = shapes[0]->depth();
+      for (int i=ldepth; i--;)
+        currentShapeL[i] = (*shapes[0])[i];
+
+      // After merging, we can pick the result of either merging left or right
+      // Here we chose the result of merging right
+      Shape* mergedShape = new Shape(maxDepth+1);
+      (*mergedShape)[0] = extent;
+      int rdepth = shapes[numberOfShapes - 1]->depth();
+      for (int i=rdepth; i--;)
+        (*mergedShape)[i+1] = (*shapes[numberOfShapes - 1])[i];
+      Extent* currentShapeR = &(*mergedShape)[1];
 
       for (int i = 1; i < numberOfShapes; i++) {
         // Merge left-to-right.  Note that due to the asymmetry of the
@@ -252,36 +268,35 @@ namespace Gecode { namespace Gist {
         // between the *previous* axis and the axis of nextShapeL.
         // This explains the correction.
 
-        Shape* oldShapeL = currentShapeL;
-        Shape* oldShapeR = currentShapeR;
-
         Shape* nextShapeL = shapes[i];
-        int nextAlphaL = getAlpha(currentShapeL, nextShapeL);
-        currentShapeL = merge(currentShapeL, nextShapeL, nextAlphaL);
-        delete oldShapeL;
+        int nextAlphaL = getAlpha(&currentShapeL[0], ldepth,
+                                  &(*nextShapeL)[0], nextShapeL->depth());
+        merge(&currentShapeL[0], &currentShapeL[0], ldepth,
+              &(*nextShapeL)[0], nextShapeL->depth(), nextAlphaL);
+        ldepth = std::max(ldepth,nextShapeL->depth());
         alphaL[i] = nextAlphaL - width;
         width = nextAlphaL;
             
         // Merge right-to-left.  Here, a correction of nextAlphaR is
         // not required.
         Shape* nextShapeR = shapes[numberOfShapes - 1 - i];
-        int nextAlphaR = getAlpha(nextShapeR, currentShapeR);
-        currentShapeR = merge(nextShapeR, currentShapeR, nextAlphaR);
-        delete oldShapeR;
+        int nextAlphaR = getAlpha(&(*nextShapeR)[0], nextShapeR->depth(),
+                                  &currentShapeR[0], rdepth);
+        merge(&currentShapeR[0], &(*nextShapeR)[0], nextShapeR->depth(),
+              &currentShapeR[0], rdepth, nextAlphaR);
+        rdepth = std::max(rdepth,nextShapeR->depth());
         alphaR[numberOfShapes - i] = nextAlphaR;
       }
-        
-      // The merged shape for the shape list is the last shape from any
-      // of the merge directions; here, we pick currentShapeR.
-      mergedShape = currentShapeR;
-      delete currentShapeL;
+
+      // The merged shape has to be adjusted to its topmost extent
+      (*mergedShape)[1].extend(- extent.l, - extent.r);
 
       // After the loop, the merged shape has the same axis as the
       // leftmost shape in the list.  What we want is to move the axis
       // such that it is the center of the axis of the leftmost shape in
       // the list and the axis of the rightmost shape.
       int halfWidth = left ? 0 : width / 2;
-      (*mergedShape)[0].move(- halfWidth);
+      (*mergedShape)[1].move(- halfWidth);
         
       // Finally, for the offset lists.  Now that the axis of the merged
       // shape is at the center of the two extreme axes, the first shape
@@ -319,24 +334,19 @@ namespace Gecode { namespace Gist {
       Extent extent(Layout::extent);
       int numberOfChildren = currentNode->getNumberOfChildren();
       Shape* shape;
-      if (numberOfChildren == -1) {
-        shape = new Shape(extent);
-      } else if (currentNode->isHidden()) {
+      if (currentNode->isHidden()) {
         shape = new Shape(&hiddenShape);
-      } else if (numberOfChildren == 0) {
+      } else if (numberOfChildren < 1) {
         shape = new Shape(extent);
       } else {
         ShapeList childShapes(numberOfChildren);
-        for (int i=0; i<numberOfChildren; i++) {
+        for (int i=numberOfChildren; i--;) {
           childShapes[i]=currentNode->getChild(i)->getShape();
         }
 
-        Shape* subtreeShape = 
-          childShapes.getMergedShape(currentNode->getStatus() == STEP);
-        (*subtreeShape)[0].extend(- extent.l, - extent.r);
-        shape = new Shape(extent, subtreeShape);
-        delete subtreeShape;
-        for (int i=0; i<numberOfChildren; i++) {
+        shape = childShapes.getMergedShape(extent,
+                                           currentNode->getStatus() == STEP);
+        for (int i=numberOfChildren; i--;) {
           currentNode->getChild(i)->
             setOffset(childShapes.getOffsetOfChild(i));
         }
