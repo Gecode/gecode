f1de49659568aad0c4c060b0f52985e7933d9004
Christian Schulte <schulte@gecode.org>
2008-08-08 16:16:58 +0000

    Regular saves memory...
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@7562 e85b7adc-8362-4630-8c63-7469d557c915

--- gecode/int/extensional.cc
+++ gecode/int/extensional.cc
@@ -41,6 +41,99 @@
 
 namespace Gecode {
 
+  namespace Int { namespace Extensional {
+
+    /// Select small types for the layered graph propagator
+    template <class View>
+    forceinline void
+    post(Space& home, ViewArray<View>& x, DFA dfa) {
+      Gecode::Support::IntType t_state_idx = 
+        Gecode::Support::s_type(x.size()*dfa.n_states());
+      Gecode::Support::IntType t_degree =
+        Gecode::Support::u_type(dfa.max_degree());
+      switch (t_state_idx) {
+      case Gecode::Support::IT_CHAR: 
+      case Gecode::Support::IT_SHRT:
+        switch (t_degree) {
+        case Gecode::Support::IT_CHAR:
+          GECODE_ES_FAIL(home,(Extensional::LayeredGraph<View,
+                               unsigned char,short int>
+                               ::post(home,x,dfa)));
+          break;
+        case Gecode::Support::IT_SHRT:
+          GECODE_ES_FAIL(home,(Extensional::LayeredGraph<View,
+                               unsigned short int,short int>
+                               ::post(home,x,dfa)));
+          break;
+        case Gecode::Support::IT_INT:
+          GECODE_ES_FAIL(home,(Extensional::LayeredGraph<View,
+                               unsigned int,short int>
+                               ::post(home,x,dfa)));
+          break;
+        case Gecode::Support::IT_LONG:
+          GECODE_ES_FAIL(home,(Extensional::LayeredGraph<View,
+                               unsigned long int,short int>
+                               ::post(home,x,dfa)));
+          break;
+        default: GECODE_NEVER;
+        }
+        break;
+      case Gecode::Support::IT_INT:
+        switch (t_degree) {
+        case Gecode::Support::IT_CHAR:
+          GECODE_ES_FAIL(home,(Extensional::LayeredGraph<View,
+                               unsigned char,int>
+                               ::post(home,x,dfa)));
+          break;
+        case Gecode::Support::IT_SHRT:
+          GECODE_ES_FAIL(home,(Extensional::LayeredGraph<View,
+                               unsigned short int,int>
+                               ::post(home,x,dfa)));
+          break;
+        case Gecode::Support::IT_INT:
+          GECODE_ES_FAIL(home,(Extensional::LayeredGraph<View,
+                               unsigned int,int>
+                               ::post(home,x,dfa)));
+          break;
+        case Gecode::Support::IT_LONG:
+          GECODE_ES_FAIL(home,(Extensional::LayeredGraph<View,
+                               unsigned long int,int>
+                               ::post(home,x,dfa)));
+          break;
+        default: GECODE_NEVER;
+        }
+        break;
+      case Gecode::Support::IT_LONG:
+        switch (t_degree) {
+        case Gecode::Support::IT_CHAR:
+          GECODE_ES_FAIL(home,(Extensional::LayeredGraph<View,
+                               unsigned char,long int>
+                               ::post(home,x,dfa)));
+          break;
+        case Gecode::Support::IT_SHRT:
+          GECODE_ES_FAIL(home,(Extensional::LayeredGraph<View,
+                               unsigned short int,long int>
+                               ::post(home,x,dfa)));
+          break;
+        case Gecode::Support::IT_INT:
+          GECODE_ES_FAIL(home,(Extensional::LayeredGraph<View,
+                               unsigned int,long int>
+                               ::post(home,x,dfa)));
+          break;
+        case Gecode::Support::IT_LONG:
+          GECODE_ES_FAIL(home,(Extensional::LayeredGraph<View,
+                               unsigned long int,long int>
+                               ::post(home,x,dfa)));
+          break;
+        default: GECODE_NEVER;
+        }
+        break;
+      default: GECODE_NEVER;
+      }
+    }
+
+  }}
+
   void
   extensional(Space& home, const IntVarArgs& x, DFA dfa, 
               IntConLevel, PropKind) {
@@ -49,8 +142,7 @@ namespace Gecode {
       throw ArgumentSame("Int::extensional");
     if (home.failed()) return;
     ViewArray<IntView> xv(home,x);
-    GECODE_ES_FAIL(home,(Extensional::LayeredGraph<IntView,unsigned int,int>
-                         ::post(home,xv,dfa)));
+    Extensional::post<IntView>(home,xv,dfa);
   }
 
   void
@@ -61,8 +153,7 @@ namespace Gecode {
       throw ArgumentSame("Int::extensional");
     if (home.failed()) return;
     ViewArray<BoolView> xv(home,x);
-    GECODE_ES_FAIL(home,(Extensional::LayeredGraph<BoolView,unsigned int,int>
-                         ::post(home,xv,dfa)));
+    Extensional::post<BoolView>(home,xv,dfa);
   }
 
   void
@@ -123,8 +214,56 @@ namespace Gecode {
     }
   }
 
-  GECODE_REGISTER3(Int::Extensional::LayeredGraph<Int::IntView,unsigned int,int>);
-  GECODE_REGISTER3(Int::Extensional::LayeredGraph<Int::BoolView,unsigned int,int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::IntView,unsigned char,short int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::IntView,unsigned short int,short int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::IntView,unsigned int,short int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::IntView,unsigned long int,short int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::IntView,unsigned char,int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::IntView,unsigned short int,int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::IntView,unsigned int,int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::IntView,unsigned long int,int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::IntView,unsigned char,long int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::IntView,unsigned short int,long int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::IntView,unsigned int,long int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::IntView,unsigned long int,long int>);
+
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::BoolView,unsigned char,short int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::BoolView,unsigned short int,short int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::BoolView,unsigned int,short int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::BoolView,unsigned long int,short int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::BoolView,unsigned char,int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::BoolView,unsigned short int,int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::BoolView,unsigned int,int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::BoolView,unsigned long int,int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::BoolView,unsigned char,long int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::BoolView,unsigned short int,long int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::BoolView,unsigned int,long int>);
+  GECODE_REGISTER3(Int::Extensional::LayeredGraph
+                   <Int::BoolView,unsigned long int,long int>);
+
 
   GECODE_REGISTER1(Int::Extensional::Basic<Int::IntView>);
   GECODE_REGISTER1(Int::Extensional::Incremental<Int::IntView>);
