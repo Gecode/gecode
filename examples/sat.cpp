e1552c3ddeb13ad91dc537f42a3eed3509f2db26
Raphael Reischuk <raphael@gecode.org>
2008-09-26 11:08:03 +0000

    * license and comments for SAT example
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@7883 e85b7adc-8362-4630-8c63-7469d557c915

--- examples/sat.cpp
+++ examples/sat.cpp
@@ -1,12 +1,42 @@
+/* -*- mode: C++; c-basic-offset: 2; indent-tabs-mode: nil -*- */
 /*
- * Example sat.cpp
- * Author: Raphael Reischuk (2008)
- * Read clauses from a dimacs file and post clause propagators.
- */
+ *  Main authors:
+ *     Raphael Reischuk <raphael@ps.uni-sb.de>
+ *
+ *  Copyright:
+ *     Raphael Reischuk, 2008
+ *
+ *  Last modified:
+ *     $Date: 2008-09-03 14:14:11 +0200 (Mi, 03 Sep 2008) $ by $Author: tack $
+ *     $Revision: 7787 $
+ *
+ *  This file is part of Gecode, the generic constraint
+ *  development environment:
+ *     http://www.gecode.org
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the
+ *  "Software"), to deal in the Software without restriction, including
+ *  without limitation the rights to use, copy, modify, merge, publish,
+ *  distribute, sublicense, and/or sell copies of the Software, and to
+ *  permit persons to whom the Software is furnished to do so, subject to
+ *  the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */        
 
 #include "examples/support.hh"
 #include "gecode/minimodel.hh"
-// #include "gecode/int/bool/clause.hpp"
 
 #include <iostream>
 #include <fstream>
@@ -14,31 +44,64 @@
 #include <sstream>
 #include <vector>
 
-const bool USE_CLAUSE_PROPAGATOR = true;
-const int READ_CLAUSES_FROM_FILE = 0;
+/**
+ * \brief %Example: CNF SAT solver
+ *
+ * SAT finds assignments of Boolean variables such
+ * that a set of clauses is satisfied or shows that
+ * no such assignment exists.
+ *
+ * This example parses a dimacs CNF file in which
+ * the constraints are specified. For each line of
+ * the file a clause propagator is posted.
+ *
+ * Format of dimacs CNF files:
+ *
+ * A dimacs file starts with comments (each line
+ * starts with c). The number of variables and the
+ * number of clauses is defined by the line 
+ *
+ *    p cnf <variables> <clauses>
+ *
+ * Each of the subsequent lines specifies a clause.
+ * A positive literal is denoted by the a positive
+ * integer, a negative literal is denoted by the
+ * corresponding negative integer. Each line is
+ * terminated by 0.
+ *
+ * c sample CNF file
+ * p cnf 3 2
+ * 3 -1 0
+ * 1 2 -1 0
+ *
+ * Benchmarks on satlib.org, for instance, 
+ * are in the dimacs CNF format.
+ *
+ * \ingroup ExProblem
+ */
 
-const std::string defaultfile = "../dimacs/dimacs.cnf";
 
 class SatOptions : public Options {
 public:
   std::string filename;
-  SatOptions(const char* s, std::string f) 
-    : Options(s), filename(f) {}
+  SatOptions(const char* s) 
+    : Options(s) {}
   /// Parse options from arguments \a argv (number is \a argc)
   void parse(int& argc, char* argv[]) {
     // Parse regular options
     Options::parse(argc,argv);
-    // No filename given
-    if (argc < 2)
-      return;
-    // Filename given, should be at position 1
+    // Filename, should be at position 1
+    if (argc == 1) {
+      help();
+      exit(1);
+    }
     filename = argv[1];
     argc--;
   }
   /// Print help message
   virtual void help(void) {
     Options::help();
-    std::cerr << "\t(string) default: " << filename << std::endl
+    std::cerr << "\t(string) " << std::endl
               << "\t\tdimacs file to parse (.cnf)" << std::endl;
   }
 };
@@ -61,38 +124,34 @@ public:
 
 class Sat : public Example {
 private:
-    // BoolVarArray for setting up branching
+    /// BoolVarArray \a BA for branching
     BoolVarArray BA;
-    std::string dimacsfile;
 
 public:
+  /// The actual problem
   Sat(const SatOptions& opt) {
-    dimacsfile = opt.filename;
     parseDIMACS(opt.filename.c_str()); 
     branch(*this, BA, INT_VAR_SIZE_MIN, INT_VAL_MIN);
   }
-    
 
-  // Constructor for Cloning
+  /// Constructor for Cloning
   Sat(bool share, Sat& s) : Example(share,s) {
-    dimacsfile = s.dimacsfile;
     BA.update(*this, share, s.BA);
   }
 
-  // Perform copying during cloning
+  /// Perform copying during cloning
   virtual Space*
   copy(bool share) {
     return new Sat(share,*this);
   }
 
-  // Print solution
+  /// Print solution
   virtual void
   print(std::ostream& os) {
     os << "\tsolution: ";
     for (int i = 0; i < BA.size(); i++)
       os << BA[i].val();
     os << std::endl;
-    checkDIMACS(dimacsfile.c_str());
   }
 
 
@@ -145,11 +204,6 @@ public:
       else if (variables > 0 && 
       ((line[0] >= '0' && line[0] <= '9') || line[0] == '-' || line[0] == ' ')) {
         c++;
-        if (READ_CLAUSES_FROM_FILE > 0 && c > READ_CLAUSES_FROM_FILE) {
-          std::getline(dimacs,line);
-          continue;
-        }
-        // printf("%8d. clause:   %s\n",c,line.c_str());
         Li* pos = new Li();
         Li* neg = new Li();
         int i = 0;
@@ -197,13 +251,12 @@ public:
         
         // Post propagators
         clause(*this, BOT_OR, positives, negatives, 1);
-        // Int::SharedBool::SharedClause::post(*this,positives,negatives);
         delete pos;
         delete neg;
       } 
       else {
         std::cerr << "format error in dimacs file" << std::endl;
-        std::cerr << "line: '" << line << "'" << std::endl;
+        std::cerr << "context: '" << line << "'" << std::endl;
         std::exit(EXIT_FAILURE);
       }
     }
@@ -215,142 +268,25 @@ public:
     printf("| Parsing done.                                     |\n");
     printf("*---------------------------------------------------*\n");
   }
-
-  bool
-  checkDIMACS(const char* f) {
-    int clauses = 0;
-    std::ifstream dimacs(f);
-    if (!dimacs) {
-      std::cerr << "error: file '" << f << "' not found" << std::endl;
-      exit(1);
-    }
-    std::string line;
-    int c = 0;
-    bool leave;
-    while (dimacs.good()) {
-      leave = false;
-      std::getline(dimacs,line);
-      if (line[0] == 'c' || line == "") {
-        // Comments (ignore them)
-      }
-      else if (line[0] == 'p' && line[1] == ' ' &&
-               line[2] == 'c' && line[3] == 'n' &&
-               line[4] == 'f' && line[5] == ' ') {
-        // Line has format "p cnf <variables> <clauses>"
-        int i = 6;
-        while (line[i] >= '0' && line[i] <= '9') {
-          i++;
-        }
-        i++;
-        while (line[i] >= '0' && line[i] <= '9') {
-          clauses = 10*clauses + line[i] - '0';
-          i++;
-        } 
-      }
-      else if ((line[0] >= '0' && line[0] <= '9') || line[0] == '-' || line[0] == ' ') {
-        // Parse regular clause
-        c++;
-        if (READ_CLAUSES_FROM_FILE > 0 && c > READ_CLAUSES_FROM_FILE) {
-          continue;
-        }
-#ifdef CL_VERBOSE
-        // printf("%8d. clause verifying:   %s\n",c,line.c_str());
-#endif
-        Li* pos = new Li();
-        Li* neg = new Li();
-        int i = 0;
-        while (line[i] != 0) {
-          if (line[i] == ' ') {
-            i++;
-            continue;
-          }
-          bool positive = true;
-          if (line[i] == '-') {
-            positive = false;
-            i++;
-          }
-          int value = 0;
-          while (line[i] >= '0' && line[i] <= '9') {
-            value = 10 * value + line[i] - '0';
-            i++;
-          }
-          if (value != 0) {
-            if (positive) 
-              pos->add(value-1);
-            else
-              neg->add(value-1);
-            i++;
-          }
-        }
-
-        // Verify clause (positives)
-        Li* tmp = pos->next();
-        i = 0;
-        while (tmp) {
-          // printf ("pos %d: %s\n",tmp->n(),BA[tmp->n()].val() == 1 ? "yes" : "no");
-          if (BA[tmp->n()].val() == 1) {
-            leave = true;
-            break;
-          }
-          tmp = tmp->next();
-        }
-        if (leave) {
-          leave = false;
-          delete pos;
-          delete neg;
-          continue;
-        }
-
-        // Verify clause (negatives)
-        tmp = neg->next();
-        i = 0;
-        while (tmp) {
-          // printf ("neg %d: %s\n",tmp->n(),BA[tmp->n()].val() == 0 ? "yes" : "no");
-          if (BA[tmp->n()].val() == 0) {
-            leave = true;
-            break;
-          }
-          tmp = tmp->next();
-        }
-        if (leave) {
-          leave = false;
-          delete pos;
-          delete neg;
-          continue;
-        }
-        else {
-          delete pos;
-          delete neg;
-          printf("error verifying %d. clause\n",c);
-          return false;
-        }
-      }
-      else {
-        std::cerr << "format error in dimacs file" << std::endl;
-        std::cerr << "line: " << line << std::endl;
-        std::exit(EXIT_FAILURE);
-      }
-    }
-    dimacs.close();
-    printf("\nVerified %d/%d clauses: ok\n",c,clauses);
-    return true;
-  }
-
 };
 
 
-// main function
+/** \brief Main-function
+ *  \relates SAT
+ */
 int main(int argc, char* argv[]) {
-  
-  SatOptions opt("SAT",defaultfile);
+
+  SatOptions opt("SAT");
   opt.parse(argc,argv);
   
+  // Check whether all arguments are successfully parsed
   if (argc > 1) {
     fprintf(stderr,"Could not parse all arguments.\n");
     opt.help();
     exit(1);
   }
-
+  
+  // Run SAT solver
   Example::run<Sat,DFS,SatOptions>(opt);
   return 0;
 }
