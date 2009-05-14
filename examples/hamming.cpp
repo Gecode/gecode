5749bfb5e40602366aa898ec3968d17d7ae791fd
Guido Tack <tack@gecode.org>
2009-05-14 00:52:04 +0000

    The parameters of hamming are now configurable
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@9097 e85b7adc-8362-4630-8c63-7469d557c915

--- examples/hamming.cpp
+++ examples/hamming.cpp
@@ -43,6 +43,42 @@
 using namespace Gecode;
 
 /**
+ * \brief %Options for %Hamming problems
+ *
+ */
+
+class HammingOptions : public Options {
+private:
+  /// Number of bits
+  Driver::UnsignedIntOption _bits;
+  /// Minimum distance
+  Driver::UnsignedIntOption _distance;
+  /// Number of symbols
+  Driver::UnsignedIntOption _size;
+  
+public:
+  /// Initialize options for example with name \a s
+  HammingOptions(const char* s, unsigned int bits0,
+                 unsigned int distance0, unsigned int size0)
+  : Options(s)
+  , _bits("-bits","word size in bits",bits0)
+  , _distance("-distance","minimum distance",distance0)
+  , _size("-size","number of symbols",size0) {
+    add(_bits);
+    add(_distance);
+    add(_size);
+  }
+  
+  /// Return number of bits
+  unsigned int bits(void) const { return _bits.value(); }
+  /// Return minimum distance
+  unsigned int distance(void) const { return _distance.value(); }
+  /// Return number of symbols
+  unsigned int size(void) const { return _size.value(); }
+  
+};
+
+/**
  * \brief %Example: Generating %Hamming codes
  *
  * Generate a Hamming code that fits in b-bit words to code n symbols where
@@ -56,18 +92,14 @@ using namespace Gecode;
  *
  */
 class Hamming : public Script {
-public:
+private:
   /// The hamming code
   SetVarArray xs;
 
-  /// Number of bits
-  static const int bits = 20;
-  /// Minimum distance between two codes
-  static const int dist = 3;
-
+public:
   /// Actual model
-  Hamming(const SizeOptions& opt) :
-    xs(*this,opt.size(),IntSet::empty,1,bits) {
+  Hamming(const HammingOptions& opt) :
+    xs(*this,opt.size(),IntSet::empty,1,opt.bits()) {
     SetVarArray cxs(*this,xs.size());
     for (int i=0; i<xs.size(); i++)
       rel(*this, xs[i], SRT_CMPL, cxs[i]);
@@ -88,7 +120,7 @@ public:
         IntVar diff2(*this,0,1204);
         cardinality(*this, xIntCy,diff1);
         cardinality(*this, yIntCx,diff2);
-        post(*this, diff1+diff2 >= dist);
+        post(*this, diff1+diff2 >= opt.distance());
       }
     }
 
@@ -120,10 +152,9 @@ public:
  */
 int
 main(int argc, char* argv[]) {
-  SizeOptions opt("Hamming");
-  opt.size(32);
+  HammingOptions opt("Hamming",20,3,32);
   opt.parse(argc,argv);
-  Script::run<Hamming,DFS,SizeOptions>(opt);
+  Script::run<Hamming,DFS,HammingOptions>(opt);
   return 0;
 }
 
