66c27c35cb67a534cda66f90eb40e070776d21e3
Christian Schulte <schulte@gecode.org>
2009-12-17 11:23:46 +0000

    Efficient bitset iteration (not yet used)
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@10205 e85b7adc-8362-4630-8c63-7469d557c915

--- gecode/support/bitset-base.hpp
+++ gecode/support/bitset-base.hpp
@@ -41,24 +41,75 @@
 
 #include <climits>
 
+#ifdef _MSC_VER
+
+#include <intrin.h>
+
+#if defined(_M_IX86)
+#pragma intrinsic(_BitScanForward)
+#define GECODE_SUPPORT_MSVC_32
+#endif
+
+#if defined(_M_X64) || defined(_M_IA64)
+#pragma intrinsic(_BitScanForward64)
+#define GECODE_SUPPORT_MSVC_64
+#endif
+
+#endif
+
 namespace Gecode { namespace Support {
+
+  class BitSetBase;
+
+  /// Date item for bitsets
+  class BitSetData {
+    friend class BitSetBase;
+  protected:
+#ifdef GECODE_SUPPORT_MSVC_64
+    /// Basetype for bits
+    typedef unsigned __int64 Base;
+#else
+    /// Basetype for bits
+    typedef unsigned long int Base;
+#endif
+    /// The bits
+    Base bits;
+    /// Bits per base
+    static const unsigned int bpb = 
+      static_cast<unsigned int>(CHAR_BIT * sizeof(Base));
+  public:
+    /// Initialize (no bit is set)
+    void init(void);
+    /// Get number of data elements for \a s bits
+    static unsigned int data(unsigned int s);
+    /// Test wether any bit with position greater or equal to \a i is set
+    bool operator ()(unsigned int i=0U) const;
+    /// Access value at bit \a i
+    bool get(unsigned int i) const;
+    /// Set bit \a i
+    void set(unsigned int i);
+    /// Clear bit \a i
+    void clear(unsigned int i);
+    /// Return next set bit with position greater or equal to \a i (there must be a bit)
+    unsigned int next(unsigned int i=0U) const;
+  };
 
   /// Basic bitset support
   class BitSetBase {
   protected:
-    /// Basetype for bits
-    typedef unsigned int Base;
     /// Bits per base
-    static const unsigned int bpb = static_cast<unsigned int>(CHAR_BIT * sizeof(Base));
+    static const unsigned int bpb = BitSetData::bpb;
     /// Size of bitset (number of bits)
     unsigned int sz;
     /// Stored bits
-    Base* data;
-    /// Get number of Base elements for \a s bits
-    static unsigned int base(unsigned int s);
+    BitSetData* data;
     /// Dispose memory for bit set
     template<class A>
     void dispose(A& a);
+    /// Access value at bit \a i (no index check)
+    bool _get(unsigned int i) const;
+    /// Set bit \a i (no index check)
+    void _set(unsigned int i);
   public:
     /// Default constructor (yields empty set)
     BitSetBase(void);
@@ -79,17 +130,102 @@ namespace Gecode { namespace Support {
     void set(unsigned int i);
     /// Clear bit \a i
     void clear(unsigned int i);
+    /// Return position greater or equal \a i of next set bit
+    unsigned int next(unsigned int i) const;
   };
 
+
+  /*
+   * Bitset data
+   *
+   */
+
+  forceinline void
+  BitSetData::init(void) {
+    bits = static_cast<Base>(0);
+  }
   forceinline unsigned int
-  BitSetBase::base(unsigned int s) {
+  BitSetData::data(unsigned int s) {
     return (s+bpb-1) / bpb;
+  }
+  forceinline bool
+  BitSetData::operator ()(unsigned int i) const {
+    return (bits >> i) != static_cast<Base>(0U);
+  }
+  forceinline bool
+  BitSetData::get(unsigned int i) const {
+    return (bits & (static_cast<Base>(1U) << i)) != static_cast<Base>(0U);
+  }
+  forceinline void
+  BitSetData::set(unsigned int i) {
+    bits |= (static_cast<Base>(1U) << i);
+  }
+  forceinline void
+  BitSetData::clear(unsigned int i) {
+    bits &= ~(static_cast<Base>(1U) << i);
+  }
+  forceinline unsigned int
+  BitSetData::next(unsigned int i) const {
+    assert(bits != static_cast<Base>(0));
+#if defined(GECODE_SUPPORT_MSVC_32)
+    assert(bpb == 32);
+    unsigned long int p;
+    _BitScanForward(&p,bits >> i);
+    return static_cast<unsigned int>(p)+i;
+#elif defined(GECODE_SUPPORT_MSVC_64)
+    assert(bpb == 64);
+    unsigned long int p;
+    _BitScanForward64(&p,bits >> i);
+    return static_cast<unsigned int>(p)+i;
+#else
+    if ((bpb == 32) || (bpb == 64)) {
+      Base b = bits >> i;
+      if ((bpb == 64) && ((b & 0xFFFFFFFFUL) == 0UL)) {
+        b >>= 32; i += 32U;
+      }
+      if ((b & 0xFFFFUL) == 0UL) {
+        b >>= 16; i += 16U;
+      }
+      if ((b & 0xFFUL) == 0UL) {
+        b >>= 8; i += 8U;
+      }
+      if ((b & 0xFUL) == 0UL) {
+        b >>= 4; i += 4U;
+      }
+      if ((b & 0x3UL) == 0UL) {
+        b >>= 2; i += 2U;
+      }
+      if ((b & 0x1UL) == 0UL) {
+        i += 1;
+      }
+      return i;
+    } else {
+      while (!get(i)) i++;
+      return i;
+    }
+#endif
+  }
+
+
+
+  /*
+   * Basic bit sets
+   *
+   */
+
+  forceinline bool
+  BitSetBase::_get(unsigned int i) const {
+    return data[i / bpb].get(i % bpb);
+  }
+  forceinline void
+  BitSetBase::_set(unsigned int i) {
+    data[i / bpb].set(i % bpb);
   }
 
   template<class A>
   forceinline void
   BitSetBase::dispose(A& a) {
-    a.template free<Base>(data,base(sz));
+    a.template free<BitSetData>(data,BitSetData::data(sz+1));
   }
 
   forceinline
@@ -99,26 +235,34 @@ namespace Gecode { namespace Support {
   template<class A>
   forceinline
   BitSetBase::BitSetBase(A& a, unsigned int s)
-    : sz(s), data(a.template alloc<Base>(base(sz))) {
-    for (unsigned int i=base(sz); i--; ) 
-      data[i] = 0;
+    : sz(s), 
+      data(a.template alloc<BitSetData>(BitSetData::data(sz+1))) {
+    for (unsigned int i = BitSetData::data(sz+1); i--; ) 
+      data[i].init();
+    // Set a bit at position sz as sentinel (for efficient next)
+    _set(sz);
   }
 
   template<class A>
   forceinline
   BitSetBase::BitSetBase(A& a, const BitSetBase& bs)
-    : sz(bs.sz), data(a.template alloc<Base>(base(bs.sz))) {
-    for (unsigned int i = base(sz); i--; ) 
+    : sz(bs.sz), 
+      data(a.template alloc<BitSetData>(BitSetData::data(sz+1))) {
+    for (unsigned int i = BitSetData::data(sz+1); i--; ) 
       data[i] = bs.data[i];
+    // Set a bit at position sz as sentinel (for efficient next)
+    _set(sz);
   }
 
   template<class A>
   forceinline void
   BitSetBase::init(A& a, unsigned int s) {
     assert((sz == 0) && (data == NULL));
-    sz=s; data=a.template alloc<Base>(base(sz));
-    for (unsigned int i=base(sz); i--; )
-      data[i] = 0;
+    sz=s; data=a.template alloc<BitSetData>(BitSetData::data(sz+1));
+    for (unsigned int i=BitSetData::data(sz+1); i--; )
+      data[i].init();
+    // Set a bit at position sz as sentinel (for efficient next)
+    _set(sz);
   }
 
   forceinline unsigned int
@@ -129,28 +273,41 @@ namespace Gecode { namespace Support {
   forceinline bool
   BitSetBase::get(unsigned int i) const {
     assert(i < sz);
-    unsigned int pos = i / bpb;
-    unsigned int bit = i % bpb;
-    return (data[pos] & (static_cast<Base>(1) << bit)) != 0;
+    return _get(i);
   }
-
   forceinline void
   BitSetBase::set(unsigned int i) {
     assert(i < sz);
-    unsigned int pos = i / bpb;
-    unsigned int bit = i % bpb;
-    data[pos] |= static_cast<Base>(1) << bit;
+    _set(i);
   }
-
   forceinline void
   BitSetBase::clear(unsigned int i) {
     assert(i < sz);
+    data[i / bpb].clear(i % bpb);
+  }
+
+  forceinline unsigned int
+  BitSetBase::next(unsigned int i) const {
+    assert(i < sz);
     unsigned int pos = i / bpb;
     unsigned int bit = i % bpb;
-    data[pos] &= ~(static_cast<Base>(1) << bit);
+    if (data[pos](bit))
+      return pos * bpb + data[pos].next(bit);
+    // The sentinel bit guarantees that this loop always terminates
+    do { 
+      pos++; 
+    } while (!data[pos]());
+    return pos * bpb + data[pos].next();
   }
 
 }}
+
+#ifdef GECODE_SUPPORT_MSVC_32
+#undef GECODE_SUPPORT_MSVC_32
+#endif
+#ifdef GECODE_SUPPORT_MSVC_64
+#undef GECODE_SUPPORT_MSVC_64
+#endif
 
 // STATISTICS: support-any
 
