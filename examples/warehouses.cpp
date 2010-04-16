a5ff453ee8f8509f11cb7ee1830393af6623075a
Christian Schulte <schulte@gecode.org>
2010-04-16 12:28:20 +0000

    Complete branching
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@10735 e85b7adc-8362-4630-8c63-7469d557c915

--- examples/warehouses.cpp
+++ examples/warehouses.cpp
@@ -132,8 +132,11 @@ public:
     // Compute total cost
     post(*this, c_fixed*sum(open) + sum(c_store) == c_total);
 
-    // Branch with largest minimum regret
+    // Branch with largest minimum regret on store cost
     branch(*this, c_store, INT_VAR_REGRET_MIN_MAX, INT_VAL_MIN);
+
+    // Branch by assigning a supplier to each store
+    branch(*this, supplier, INT_VAR_NONE, INT_VAL_MIN);
   }
   /// Return solution cost
   virtual IntVar cost(void) const {
