f0a9f207b45860724258713924fd73aeb6bdbd50
Christian Schulte <schulte@gecode.org>
2010-04-17 11:42:13 +0000

    Better BIBD
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@10748 e85b7adc-8362-4630-8c63-7469d557c915

--- examples/bibd.cpp
+++ examples/bibd.cpp
@@ -100,63 +100,33 @@ protected:
   /// Matrix of Boolean variables
   BoolVarArray _p;
 public:
-  /// Access variable by row and column
-  BoolVar& p(int i, int j) {
-    // row, column
-    return _p[i*opt.b+j];
-  }
-  /// Access variable by row and column
-  const BoolVar& p(int i, int j) const {
-    // row, column
-    return _p[i*opt.b+j];
-  }
   /// Actual model
   BIBD(const BIBDOptions& o)
     : opt(o), _p(*this,opt.v*opt.b,0,1) {
+    Matrix<BoolVarArray> p(_p,opt.b,opt.v);
+
     // r ones per row
-    for (int i=opt.v; i--; ) {
-      BoolVarArgs row(opt.b);
-      for (int j=opt.b; j--; )
-        row[j] = p(i,j);
-      linear(*this, row, IRT_EQ, opt.r);
-    }
+    for (int i=0; i<opt.v; i++)
+      linear(*this, p.row(i), IRT_EQ, opt.r);
+
     // k ones per column
-    for (int j=opt.b; j--; ) {
-      BoolVarArgs col(opt.v);
-      for (int i=opt.v; i--; )
-        col[i] = p(i,j);
-      linear(*this, col, IRT_EQ, opt.k);
-    }
+    for (int j=0; j<opt.b; j++)
+      linear(*this, p.col(j), IRT_EQ, opt.k);
+
     // Exactly lambda ones in scalar product between two different rows
     for (int i1=0; i1<opt.v; i1++)
       for (int i2=i1+1; i2<opt.v; i2++) {
         BoolVarArgs row(opt.b);
-        for (int j=opt.b; j--; ) {
-          BoolVar b(*this,0,1);
-          rel(*this,p(i1,j),BOT_AND,p(i2,j),b);
-          row[j] = b;
-        }
+        for (int j=0; j<opt.b; j++)
+          row[j] = post(*this, p(j,i1) && p(j,i2));
         linear(*this, row, IRT_EQ, opt.lambda);
       }
 
-    for (int i=1;i<opt.v;i++) {
-      BoolVarArgs row1(opt.b);
-      BoolVarArgs row2(opt.b);
-      for (int j=opt.b; j--; ) {
-        row1[j] = p(i-1,j);
-        row2[j] = p(i,j);
-      }
-      rel(*this, row1, IRT_GQ, row2);
-    }
-    for (int j=1;j<opt.b;j++) {
-      BoolVarArgs col1(opt.v);
-      BoolVarArgs col2(opt.v);
-      for (int i=opt.v; i--; ) {
-        col1[i] = p(i,j-1);
-        col2[i] = p(i,j);
-      }
-      rel(*this, col1, IRT_GQ, col2);
-    }
+    // Symmetry breaking
+    for (int i=1; i<opt.v; i++)
+      rel(*this, p.row(i-1), IRT_GQ, p.row(i));
+    for (int j=1; j<opt.b; j++)
+      rel(*this, p.col(j-1), IRT_GQ, p.col(j));
 
     branch(*this, _p, INT_VAR_NONE, INT_VAL_MIN);
   }
@@ -167,10 +137,11 @@ public:
     os << "\tBIBD("
        << opt.v << "," << opt.k << ","
        << opt.lambda << ")" << std::endl;
-    for (int i = 0; i < opt.v; i++) {
+    Matrix<BoolVarArray> p(_p,opt.b,opt.v);
+    for (int i = 0; i<opt.v; i++) {
       os << "\t\t";
-      for (int j = 0; j< opt.b; j++)
-        os << p(i,j) << " ";
+      for (int j = 0; j<opt.b; j++)
+        os << p(j,i) << " ";
       os << std::endl;
     }
     os << std::endl;
