7798288a27fa77c933fee24d210d4ea097fcb098
Guido Tack <tack@gecode.org>
2010-04-21 16:03:30 +0000

    Cleaned up golf example a bit
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@10786 e85b7adc-8362-4630-8c63-7469d557c915

--- examples/golf.cpp
+++ examples/golf.cpp
@@ -41,30 +41,30 @@
 
 using namespace Gecode;
 
-/**
- * \name Parameters for golf tournaments
- *
- * \relates Golf
- */
-//@{
-/// %Tournament parameters
-struct Tournament {
-  /// Number of groups
-  int groups;
-  /// Number of players in each group
-  int playersInGroup;
-  /// Number of weeks
-  int weeks;
+/// \brief %Options for %Golf example
+class GolfOptions : public Options {
+protected:
+  Driver::IntOption _w; //< Number of weeks
+  Driver::IntOption _g; //< Number of groups
+  Driver::IntOption _s; //< Number of players per group
+public:
+  /// Constructor
+  GolfOptions(void)
+    : Options("Golf"),
+      _w("-w","number of weeks",9),
+      _g("-g","number of groups",8),
+      _s("-s","number of players per group",4) {
+    add(_w);
+    add(_g);
+    add(_s);
+  }
+  /// Return number of weeks
+  int w(void) const { return _w.value(); }
+  /// Return number of groups
+  int g(void) const { return _g.value(); }
+  /// Return number of players per group
+  int s(void) const { return _s.value(); }
 };
-/// Tournaments
-static const Tournament t[]=
-  { {8,4,9},
-    {5,3,7},
-    {4,3,2}
-  };
-/// Number of tournaments
-static const unsigned int n_examples = sizeof(t) / sizeof(Tournament);
-//@}
 
 /**
  * \brief %Example: %Golf tournament
@@ -81,73 +81,37 @@ public:
     MODEL_PLAIN,   ///< A simple model
     MODEL_SYMMETRY ///< Model with symmetry breaking
   };
-  enum {
-    PROP_PLAIN,    ///< Propagation using intersection propagators
-    PROP_DECOMPOSE ///< Propagation using union and complement propagators
-  };
-  int groups;          ///< Number of groups in a week
-  int playersInGroup;  ///< Number of players in a group
-  int weeks;           ///< Number of weeks
-  int players;         ///< Overall number of players
+  int g; ///< Number of groups in a week
+  int s; ///< Number of players in a group
+  int w; ///< Number of weeks
 
   /// The sets representing the groups
-  SetVarArray groupsS;
+  SetVarArray groups;
 
-  /// Return group number \a g in week \a w
-  SetVar& group(int w, int g) {
-    return groupsS[w*groups+g];
-  }
-  /// Return group number \a g in week \a w
-  const SetVar& group(int w, int g) const {
-    return groupsS[w*groups+g];
+  void atMostOne(SetVar x, SetVar y) {
+    SetVar z(*this,IntSet::empty,0,g*s-1,0,1);
+    rel(*this, x, SOT_INTER, y, SRT_EQ, z);
   }
 
   /// Actual model
-  Golf(const SizeOptions& opt) :
-    groups(t[opt.size()].groups),
-    playersInGroup(t[opt.size()].playersInGroup),
-    weeks(t[opt.size()].weeks),
-    players(groups*playersInGroup),
-    groupsS(*this,groups*weeks,IntSet::empty,0,players-1,
-            playersInGroup,playersInGroup) {
-
-    SetVar allPlayers(*this, 0, players-1, 0, players-1);
+  Golf(const GolfOptions& opt) : g(opt.g()), s(opt.s()), w(opt.w()),
+    groups(*this,g*w,IntSet::empty,0,g*s-1,s,s) {
+    int players = g*s;
+    Matrix<SetVarArray> schedule(groups,w,g);
 
     // Groups in one week must be disjoint
-    for (int w=0; w<weeks; w++) {
-      SetVarArgs p(groups);
-      for (int g=0; g < groups; g++)
-               p[g] = group(w,g);
-
-      rel(*this,SOT_DUNION,p,allPlayers);
+    SetVar allPlayers(*this, 0,players-1, 0,players-1);
+    for (int i=0; i<w; i++) {
+      rel(*this,SOT_DUNION,schedule.col(i),allPlayers);
     }
 
     // No two golfers play in the same group more than once
-    for (int w=0; w<weeks; w++) {
-      for (int g=0; g<groups; g++) {
-        SetVar v = group(w,g);
-        SetVar vcompl;
-        if (opt.propagation() == PROP_DECOMPOSE) {
-          vcompl = SetVar(*this,IntSet::empty,
-                          Set::Limits::min,Set::Limits::max);
-          rel(*this, v, SRT_CMPL, vcompl);
-        }
-        for (int i=(w+1)*groups; i<weeks*groups; i++) {
-          if (opt.propagation() == PROP_PLAIN) {
-            SetVar atMostOne(*this,IntSet::empty,0,players-1,0,1);
-            rel(*this, v, SOT_INTER, groupsS[i], SRT_EQ, atMostOne);
-          } else {
-            SetVar atMostOneC(*this,IntSet::empty,
-                              Set::Limits::min,
-                              Set::Limits::max,
-                              Set::Limits::card-1,
-                              Set::Limits::card);
-            SetVar groupsSiC(*this,IntSet::empty,
-                             Set::Limits::min,Set::Limits::max);
-            rel(*this, groupsS[i], SRT_CMPL, groupsSiC);
-            rel(*this, vcompl, SOT_UNION, groupsSiC, SRT_EQ, atMostOneC);
-          }
-        }
+    for (int i=0; i<w; i++) {
+      for (int j=0; j<g; j++) {
+        SetVarArgs rest(schedule.slice(0,w,j+1,g));
+        rest += schedule.slice(i+1,w,j,j+1);
+        for (int k=rest.size(); k--;)
+          atMostOne(schedule(j,j),rest[k]);
       }
     }
 
@@ -162,42 +126,33 @@ public:
 
       // Redundant constraint:
       // in each week, one player plays in only one group
-      for (int w=0; w < weeks; w++) {
+      for (int i=0; i<w; i++) {
          for (int p=0; p < players; p++) {
-           BoolVarArgs bs(groups);
-           for (int g=0; g<groups; g++) {
-            BoolVar b(*this,0,1);
-            dom(*this, group(w,g), SRT_SUP, p, b);
-            bs[g] = b;
-          }
-           linear(*this, bs, IRT_EQ, 1);
+           BoolVarArray b(*this,g,0,1);
+           for (int j=0; j<g; j++)
+             dom(*this, schedule(i,j), SRT_SUP, p, b[j]);
+           linear(*this, b, IRT_EQ, 1);
          }
        }
 
-      // Redundant constraint:
-      // any two groups has at most one player in common
-      atmostOne(*this, groupsS, playersInGroup);
-
       // Symmetry breaking: order groups
-      for (int w=0; w<weeks; w++) {
-        for (int g=0; g<groups-1; g++) {
-          IntVar minG1(*this, 0, players-1);
-          IntVar minG2(*this, 0, players-1);
-          SetVar g1 = group(w,g);
-          SetVar g2 = group(w,g+1);
-          min(*this, g1, minG1);
-          min(*this, g2, minG2);
-          rel(*this, minG1, IRT_LE, minG2);
+      for (int i=0; i<w; i++) {
+        for (int j=0; j<g-1; j++) {
+          IntVar min1(*this, 0, players-1);
+          IntVar min2(*this, 0, players-1);
+          min(*this, schedule(i,j), min1);
+          min(*this, schedule(i,j+1), min2);
+          rel(*this, min1, IRT_LE, min2);
         }
       }
 
       // Symmetry breaking: order weeks
       // minElem(group(w,0)\{0}) < minElem(group(w+1,0)\{0})
-      for (int w=0; w<weeks-1; w++) {
+      for (int i=0; i<w-1; i++) {
         SetVar g1(*this, IntSet::empty, 1, players-1);
         SetVar g2(*this, IntSet::empty, 1, players-1);
-        rel(*this, g1, SOT_DUNION, IntSet(0,0), SRT_EQ, group(w,0));
-        rel(*this, g2, SOT_DUNION, IntSet(0,0), SRT_EQ, group(w+1,0));
+        rel(*this, g1, SOT_DUNION, IntSet(0,0), SRT_EQ, schedule(i,0));
+        rel(*this, g2, SOT_DUNION, IntSet(0,0), SRT_EQ, schedule(i+1,0));
         IntVar minG1(*this, 0, players-1);
         IntVar minG2(*this, 0, players-1);
         min(*this, g1, minG1);
@@ -206,59 +161,55 @@ public:
       }
 
       // Initialize the dual variables:
-      // groupsSInv[w*players+p] is player p's group in week w
-      IntVarArray groupsSInv(*this, weeks*players, 0, groups-1);
-      for (int w=0; w<weeks; w++) {
+      // gInv(i,p) is player p's group in week i
+      Matrix<IntVarArray> gInv(IntVarArray(*this, w*players, 0, g-1),
+                               w,players);
+      for (int i=0; i<w; i++) {
         for (int p=0; p<players; p++) {
-          SetVar thisPlayer(*this, p,p, 0, players-1);
-          SetVarArgs thisWeek(groups);
-          for (int g=0; g<groups; g++)
-            thisWeek[g] = group(w,g);
-          element(*this, thisWeek, groupsSInv[w*players+p], thisPlayer);
+          SetVar player(*this, p,p, 0, players-1);
+          element(*this, schedule.col(i), gInv(i,p),player);
         }
       }
-
+      
       // Symmetry breaking: order players
       // For all p<groups : groupsSInv[w*players+p] <= p
-      for (int w=0; w<weeks; w++)
-        for (int p=0; p<groups; p++)
-          rel(*this, groupsSInv[w*players+p], IRT_LQ, p);
+      for (int i=0; i<w; i++)
+        for (int p=0; p<g; p++)
+          rel(*this, gInv(i,p), IRT_LQ, p);
     }
 
-    branch(*this, groupsS, SET_VAR_MIN_MIN, SET_VAL_MIN_INC);
+    branch(*this, groups, SET_VAR_MIN_MIN, SET_VAL_MIN_INC);
   }
 
   /// Print solution
   virtual void
   print(std::ostream& os) const {
     os << "Tournament plan" << std::endl;
-
-    for (int w=0; w<weeks; w++) {
-      os << "Week " << w << ": " << std::endl << "    ";
-      for (int g=0; g<groups; g++) {
-        if (group(w,g).assigned()) {
+    Matrix<SetVarArray> schedule(groups,w,g);
+    for (int i=0; i<w; i++) {
+      os << "Week " << i << ": " << std::endl << "    ";
+      for (int j=0; j<g; j++) {
+        if (schedule(i,j).assigned()) {
           bool first = true;
           os << "(";
-          for (SetVarGlbValues glb(group(w,g)); glb(); ++glb) {
+          for (SetVarGlbValues glb(schedule(i,j)); glb(); ++glb) {
             if (first) first = false; else os << " ";
             os << glb.val();
           }
           os << ")";
         } else {
-          os << "(" << group(w,g) << ")";
+          os << "(" << schedule(i,j) << ")";
         }
-        if (g < groups-1) os << " ";
-        if (g > 0 && g % 4 == 0) os << std::endl << "    ";
+        if (j < g-1) os << " ";
+        if (j > 0 && j % 4 == 0) os << std::endl << "    ";
       }
       os << std::endl;
     }
   }
 
   /// Constructor for copying \a s
-  Golf(bool share, Golf& s) : Script(share,s),
-      groups(s.groups), playersInGroup(s.playersInGroup),
-      weeks(s.weeks), players(s.players) {
-    groupsS.update(*this, share, s.groupsS);
+  Golf(bool share, Golf& s) : Script(share,s), g(s.g), s(s.s), w(s.w) {
+    groups.update(*this, share, s.groups);
   }
   /// Copy during cloning
   virtual Space*
@@ -272,24 +223,14 @@ public:
  */
 int
 main(int argc, char* argv[]) {
-  SizeOptions opt("Golf");
+  GolfOptions opt;
   opt.model(Golf::MODEL_PLAIN);
   opt.model(Golf::MODEL_PLAIN, "none", "no symmetry breaking");
   opt.model(Golf::MODEL_SYMMETRY, "symmetry", "static symmetry breaking");
-  opt.propagation(Golf::PROP_PLAIN);
-  opt.propagation(Golf::PROP_PLAIN, "plain", "intersection constraints");
-  opt.propagation(Golf::PROP_DECOMPOSE, "decompose",
-                  "union and complement constraints");
   opt.solutions(1);
   opt.parse(argc,argv);
-  if (opt.size() >= n_examples) {
-    std::cerr << "Error: size must be between 0 and " << n_examples - 1
-              << std::endl;
-    return 1;
-  }
-  Script::run<Golf,DFS,SizeOptions>(opt);
+  Script::run<Golf,DFS,GolfOptions>(opt);
   return 0;
 }
 
 // STATISTICS: example-any
-
