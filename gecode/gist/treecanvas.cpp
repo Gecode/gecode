3073944c6153241907e2a2953ddbee4fcf409bf0
Guido Tack <tack@gecode.org>
2010-05-11 09:00:07 +0000

    Catch exceptions in inspectors and print proper error messages
    git-svn-id: file:///Users/tack/GecodeGitMigration/gecode-svn-mirror/gecode/trunk@10936 e85b7adc-8362-4630-8c63-7469d557c915

--- gecode/gist/treecanvas.cpp
+++ gecode/gist/treecanvas.cpp
@@ -606,91 +606,122 @@ namespace Gecode { namespace Gist {
       return;
     }
 
-    switch (currentNode->getStatus()) {
-    case UNDETERMINED:
-        {
-          unsigned int kids =  
-            currentNode->getNumberOfChildNodes(*na,curBest,stats,c_d,a_d);
-          int depth = -1;
-          for (VisualNode* p = currentNode; p != NULL; p = p->getParent())
-            depth++;
-          if (kids > 0)
-            depth++;
-          stats.maxDepth =
-            std::max(stats.maxDepth, depth);
-          if (currentNode->getStatus() == SOLVED) {
-            assert(currentNode->hasWorkingSpace());
-            emit solution(currentNode->getWorkingSpace());
-            currentNode->purge();
-          }
-          emit statusChanged(currentNode,stats,true);
-          for (int i=0; i<moveInspectors.size(); i++) {
-            if (moveInspectors[i].second) {
-              moveInspectors[i].first->inspect(*currentNode->getWorkingSpace());
+    int failedInspectorType = -1;
+    int failedInspector = -1;
+    try {
+      switch (currentNode->getStatus()) {
+      case UNDETERMINED:
+          {
+            unsigned int kids =  
+              currentNode->getNumberOfChildNodes(*na,curBest,stats,c_d,a_d);
+            int depth = -1;
+            for (VisualNode* p = currentNode; p != NULL; p = p->getParent())
+              depth++;
+            if (kids > 0)
+              depth++;
+            stats.maxDepth =
+              std::max(stats.maxDepth, depth);
+            if (currentNode->getStatus() == SOLVED) {
+              assert(currentNode->hasWorkingSpace());
+              emit solution(currentNode->getWorkingSpace());
+              currentNode->purge();
+            }
+            emit statusChanged(currentNode,stats,true);
+            for (int i=0; i<moveInspectors.size(); i++) {
+              if (moveInspectors[i].second) {
+                failedInspectorType = 0;
+                failedInspector = i;
+                moveInspectors[i].first->
+                  inspect(*currentNode->getWorkingSpace());
+                failedInspectorType = -1;
+              }
             }
           }
+          break;
+      case FAILED:
+      case STOP:
+      case UNSTOP:
+      case BRANCH:
+      case SOLVED:
+        {
+          // SizeCursor sc(currentNode);
+          // PreorderNodeVisitor<SizeCursor> pnv(sc);
+          // int nodes = 1;
+          // while (pnv.next()) { nodes++; }
+          // std::cout << "sizeof(VisualNode): " << sizeof(VisualNode)
+          //           << std::endl;
+          // std::cout << "Size: " << (pnv.getCursor().s)/1024 << std::endl;
+          // std::cout << "Nodes: " << nodes << std::endl;
+          // std::cout << "Size / node: " << (pnv.getCursor().s)/nodes
+          //           << std::endl;
+
+          Space* curSpace;
+        
+          if (fix) {
+            if (currentNode->isRoot() && currentNode->getStatus() == FAILED)
+              break;
+            curSpace = currentNode->getSpace(curBest,c_d,a_d);
+            if (currentNode->getStatus() == SOLVED &&
+                curSpace->status() != SS_SOLVED) {
+              // in the presence of weakly monotonic propagators, we may have to
+              // use search to find the solution here
+              Space* dfsSpace = Gecode::dfs(curSpace);
+              delete curSpace;
+              curSpace = dfsSpace;
+            }          
+          } else {
+            if (currentNode->isRoot())
+              break;
+            VisualNode* p = currentNode->getParent();
+            curSpace = p->getSpace(curBest,c_d,a_d);
+            switch (curSpace->status()) {
+            case SS_SOLVED:
+            case SS_FAILED:
+              break;
+            case SS_BRANCH:
+              curSpace->commit(*p->getChoice(), currentNode->getAlternative());
+              break;
+            default:
+              GECODE_NEVER;
+            }
+          }
+
+          if (inspectorNo==-1) {
+            for (int i=0; i<doubleClickInspectors.size(); i++) {
+              if (doubleClickInspectors[i].second) {
+                failedInspectorType = 1;
+                failedInspector = i;
+                doubleClickInspectors[i].first->inspect(*curSpace);
+                failedInspectorType = -1;
+              }
+            }
+          } else {
+            failedInspectorType = 1;
+            failedInspector = inspectorNo;
+            doubleClickInspectors[inspectorNo].first->inspect(*curSpace);
+            failedInspectorType = -1;
+          }
+          delete curSpace;
         }
         break;
-    case FAILED:
-    case STOP:
-    case UNSTOP:
-    case BRANCH:
-    case SOLVED:
-      {
-        // SizeCursor sc(currentNode);
-        // PreorderNodeVisitor<SizeCursor> pnv(sc);
-        // int nodes = 1;
-        // while (pnv.next()) { nodes++; }
-        // std::cout << "sizeof(VisualNode): " << sizeof(VisualNode)
-        //           << std::endl;
-        // std::cout << "Size: " << (pnv.getCursor().s)/1024 << std::endl;
-        // std::cout << "Nodes: " << nodes << std::endl;
-        // std::cout << "Size / node: " << (pnv.getCursor().s)/nodes
-        //           << std::endl;
-
-        Space* curSpace;
-        
-        if (fix) {
-          if (currentNode->isRoot() && currentNode->getStatus() == FAILED)
-            break;
-          curSpace = currentNode->getSpace(curBest,c_d,a_d);
-          if (currentNode->getStatus() == SOLVED &&
-              curSpace->status() != SS_SOLVED) {
-            // in the presence of weakly monotonic propagators, we may have to
-            // use search to find the solution here
-            Space* dfsSpace = Gecode::dfs(curSpace);
-            delete curSpace;
-            curSpace = dfsSpace;
-          }          
-        } else {
-          if (currentNode->isRoot())
-            break;
-          VisualNode* p = currentNode->getParent();
-          curSpace = p->getSpace(curBest,c_d,a_d);
-          switch (curSpace->status()) {
-          case SS_SOLVED:
-          case SS_FAILED:
-            break;
-          case SS_BRANCH:
-            curSpace->commit(*p->getChoice(), currentNode->getAlternative());
-            break;
-          default:
-            GECODE_NEVER;
-          }
-        }
-
-        if (inspectorNo==-1) {
-          for (int i=0; i<doubleClickInspectors.size(); i++) {
-            if (doubleClickInspectors[i].second) {
-              doubleClickInspectors[i].first->inspect(*curSpace);
-            }
-          }
-        } else {
-          doubleClickInspectors[inspectorNo].first->inspect(*curSpace);          
-        }
-        delete curSpace;
       }
-      break;
+    } catch (Exception e) {
+      switch (failedInspectorType) {
+      case 0:
+        std::cerr << "Exception in move inspector "
+                  << failedInspector;
+        break;
+      case 1:
+        std::cerr << "Exception in double-click inspector "
+                  << failedInspector;
+        break;
+      default:
+        std::cerr << "Exception ";
+        break;
+      }
+      std::cerr << ": " << e.what() << "." << std::endl;
+      std::cerr << "Stopping..." << std::endl;
+      std::exit(EXIT_FAILURE);
     }
 
     currentNode->dirtyUp();
@@ -705,22 +736,49 @@ namespace Gecode { namespace Gist {
 
   void
   TreeCanvas::inspectSolution(const Space* s) {
-    Space* c = NULL;
-    for (int i=0; i<solutionInspectors.size(); i++) {
-      if (solutionInspectors[i].second) {
-        if (c == NULL)
-          c = s->clone();
-        solutionInspectors[i].first->inspect(*c);
+    int failedInspectorType = -1;
+    int failedInspector = -1;
+    try {
+      Space* c = NULL;
+      for (int i=0; i<solutionInspectors.size(); i++) {
+        if (solutionInspectors[i].second) {
+          if (c == NULL)
+            c = s->clone();
+          failedInspectorType = 1;
+          failedInspector = i;
+          solutionInspectors[i].first->inspect(*c);
+          failedInspectorType = -1;
+        }
       }
-    }
-    for (int i=0; i<moveInspectors.size(); i++) {
-      if (moveInspectors[i].second) {
-        if (c == NULL)
-          c = s->clone();
-        moveInspectors[i].first->inspect(*c);
+      for (int i=0; i<moveInspectors.size(); i++) {
+        if (moveInspectors[i].second) {
+          if (c == NULL)
+            c = s->clone();
+          failedInspectorType = 0;
+          failedInspector = i;
+          moveInspectors[i].first->inspect(*c);
+          failedInspectorType = -1;
+        }
       }
+      delete c;
+    } catch (Exception e) {
+      switch (failedInspectorType) {
+      case 0:
+        std::cerr << "Exception in move inspector "
+                  << failedInspector;
+        break;
+      case 1:
+        std::cerr << "Exception in solution inspector "
+                  << failedInspector;
+        break;
+      default:
+        std::cerr << "Exception ";
+        break;
+      }
+      std::cerr << ": " << e.what() << "." << std::endl;
+      std::cerr << "Stopping..." << std::endl;
+      std::exit(EXIT_FAILURE);      
     }
-    delete c;
   }
 
   void
@@ -1212,7 +1270,14 @@ namespace Gecode { namespace Gist {
         if (moveInspectors[i].second) {
           if (curSpace == NULL)
             curSpace = n->getSpace(curBest,c_d,a_d);
-          moveInspectors[i].first->inspect(*curSpace);
+          try {
+            moveInspectors[i].first->inspect(*curSpace);
+          } catch (Exception e) {
+            std::cerr << "Exception in move inspector " << i
+                      << ": " << e.what() << "." << std::endl;
+            std::cerr << "Stopping..." << std::endl;
+            std::exit(EXIT_FAILURE);
+          }
         }
       }
     }
