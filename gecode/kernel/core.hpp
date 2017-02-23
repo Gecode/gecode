Gecode/gecode

commit 48943c04a62bc0dec48db9cb39d4f32f941bad47

Fix afc on assigned variables
git-svn-id: svn+ssh://svn.gecode.org/srv/gecode/svn/gecode/trunk@15475 64335634-5103-0410-b293-fc3d331e086d

gecode/kernel/core.hpp
@@ -4348,6 +4348,10 @@ namespace Gecode {
     b.base = NULL;
     // Must be 0 such that degree works
     entries = 0;
+    // Must be NULL such that afc works
+    for (PropCond pc=1; pc<pc_max+2; pc++)
+      idx(pc) = 0;
+    free_and_bits = 0;
   }
 
   template<class VIC>
@@ -4393,8 +4397,6 @@ namespace Gecode {
   template<class VIC>
   void
   VarImp<VIC>::_fail(Space& home) {
-    if (b.base == NULL)
-      return;
     /*
      * An advisor that is executed might remove itself due to subsumption.
      * As entries are removed from front to back, the advisors must

gecode/kernel/gpi.hpp
@@ -74,8 +74,6 @@ namespace Gecode {
     /// The object containing the shared information
     class Object : public HeapAllocated {
     private:
-      /// One statically included Block
-      Block  fst;
       /// The current block
       Block* b;
       /// The inverse decay factor
@@ -141,7 +139,7 @@ namespace Gecode {
 
   forceinline
   GPI::Object::Object(void)
-    : b(&fst), invd(1.0), pid(0U), use_cnt(1U) {}
+    : b(new Block), invd(1.0), pid(0U), use_cnt(1U) {}
 
   forceinline void
   GPI::Object::inc(void) {
@@ -155,8 +153,8 @@ namespace Gecode {
     m.acquire();
     c.afc = invd * (c.afc + 1.0);
     if (c.afc > Config::rescale_limit)
-      for (Block* b = &fst; b != NULL; b = b->next)
-        b->rescale();
+      for (Block* i = b; i != NULL; i = i->next)
+        i->rescale();
     m.release();
   }
 
@@ -194,7 +192,7 @@ namespace Gecode {
   GPI::Object::dispose(void) {
     m.acquire();
     if (--use_cnt == 0) {
-      Block* n = fst.next;
+      Block* n = b;
       while (n != NULL) {
         Block* d = n;
         n = n->next;

commit e8b05ce82848eb28d9c9ddd89b05e2e79222bcad

Found super bug introduced with r15475
git-svn-id: svn+ssh://svn.gecode.org/srv/gecode/svn/gecode/trunk@15481 64335634-5103-0410-b293-fc3d331e086d

gecode/kernel/core.hpp
@@ -4340,7 +4340,7 @@ namespace Gecode {
   VarImp<VIC>::cancel(Space& home) {
     unsigned int n_sub = degree();
     home.pc.p.n_sub -= n_sub;
-    unsigned int n = (free_and_bits >> VIC::free_bits) + n_sub;
+    unsigned int n = (free_and_bits >> free_bits) + n_sub;
     home.free<ActorLink*>(b.base,n);
     // Must be NULL such that cloning works
     b.base = NULL;
@@ -4349,7 +4349,7 @@ namespace Gecode {
     // Must be NULL such that afc works
     for (PropCond pc=1; pc<pc_max+2; pc++)
       idx(pc) = 0;
-    free_and_bits = 0;
+    free_and_bits &= (1 << free_bits) - 1;
   }
 
   template<class VIC>