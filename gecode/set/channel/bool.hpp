a65b6a1f7711ea71a891b6edc2a9cb6e75325947
Guido Tack <guido.tack@monash.edu>
2019-07-29 11:45:18 +1000

    Fix bool-set channel constraint when set variable is already fixed during posting

--- gecode/set/channel/bool.hpp
+++ gecode/set/channel/bool.hpp
@@ -98,9 +98,9 @@ namespace Gecode { namespace Set { namespace Channel {
       View::schedule(home, *this, y.assigned() ? ME_SET_VAL : ME_SET_BB);
       if (y.assigned()) {
         if (y.glbSize()==static_cast<unsigned int>(y.glbMax()-y.glbMin()+1)) {
-          new (&delta) SetDelta(y.glbMin(),y.glbMax(),1,0);
+          new (&delta) SetDelta(y.glbMin(),y.glbMax(),2,0);
         } else {
-          new (&delta) SetDelta(2,0,1,0);
+          new (&delta) SetDelta(2,0,2,0);
         }
       }
       (void) new (home) IndexAdvisor(home,*this,co,-1);
