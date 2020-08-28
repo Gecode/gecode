bf1b37f04db7c911a5b24d7e3716ac145ec780ae
Mikael Zayenz Lagerkvist <lagerkvist@gecode.org>
2020-08-28 19:17:59 +0200

    Fix path compression in disjoint sets for sorted
    The OfflineMin data structure used for the sorted constraint is a
    union-find disjoint sets data structure. The find_pc method was
    supposed to do path compression by storing all the nodes seen during
    traversal of the tree, and then later setting all the intermediaries
    to the root.
    
    The code did not work as intended, it never did any path compression
    at all.
    
    No change in test behaviour for sorted (still works, and no changee in
    time).

--- gecode/int/sorted/sortsup.hpp
+++ gecode/int/sorted/sortsup.hpp
@@ -153,14 +153,14 @@ namespace Gecode { namespace Int { namespace Sorted {
     OfflineMin(OfflineMinItem[], int[], int);
     /**
      *  Find the set x belongs to
-     *  (wihtout path compression)
+     *  (without path compression)
      */
-    int  find(int x);
+    int find(int x);
     /**
      *  Find the set x belongs to
      *  (using path compression)
      */
-    int  find_pc(int x);
+    int find_pc(int x);
     /// Unite two sets \a a and \a b and label the union with \a c
     void unite(int a, int b, int c);
     /// Initialization of the datastructure
@@ -196,7 +196,7 @@ namespace Gecode { namespace Int { namespace Sorted {
   OfflineMin::find_pc(int x){
     int vsize = 0;
     while (sequence[x].parent != x) {
-      vertices[x] = x;
+      vertices[vsize++] = x;
       x = sequence[x].parent;
     }
     // x is now the root of the tree
