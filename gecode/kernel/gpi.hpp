d7fe45d298863409196288c6104877722199370a
Yuri Victorovich <yuri@FreeBSD.org>
2021-05-09 11:10:33 -0700

    Fix c++20 compatibility issue in fetch_add invocation in gpi.hh
    Also add a missing argument 'arg' to fetch_add.

--- gecode/kernel/gpi.hpp
+++ gecode/kernel/gpi.hpp
@@ -186,7 +186,11 @@ namespace Gecode { namespace Kernel {
     }
     c = &b->info[--b->free];
     m.release();
-    c->init(npid.fetch_add(std::memory_order::memory_order_seq_cst),gid);
+#if _LIBCPP_STD_VER <= 17
+    c->init(npid.fetch_add(1, std::memory_order::memory_order_seq_cst),gid);
+#else
+    c->init(npid.fetch_add(1, std::memory_order_seq_cst),gid);
+#endif
     return c;
   }
 
